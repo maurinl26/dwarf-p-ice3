c2615f70816259b0d931113993b9cb183c550032
Loic Maurin <maurin.loic.ac@gmail.com>
2024-01-09 13:29:13 +0100

    cph refactored with externals

--- src/phyex_gt4py/functions/ice_adjust.py
+++ src/phyex_gt4py/functions/ice_adjust.py
@@ -8,34 +8,32 @@ from gt4py.cartesian.gtscript import Field, function
 def vaporisation_latent_heat(
     t: Field,
 ):
-    
+
     from __externals__ import lvtt, cpv, Cl, tt
-    
+
     return lvtt + (cpv - Cl) * (t - tt)
+
 
 @function
 def sublimation_latent_heat(
     t: Field,
 ):
-    
+
     from __externals__ import lstt, cpv, Ci, tt
-    
+
     return lstt + (cpv - Ci) * (t - tt)
 
 
 @function
-def _cph(
+def cph(
     rv: Field["float"],
     rc: Field["float"],
     ri: Field["float"],
     rr: Field["float"],
     rs: Field["float"],
     rg: Field["float"],
-    cpd: float,
-    cpv: float,
-    Cl: float,
-    Ci: float,
 ):
-    cph = cpd + cpv * rv + Cl * (rc + rr) + Ci * (ri + rs + rg)
 
-    return cph
+    from __externals__ import cpd, cpv, Cl, Ci
+
+    return cpd + cpv * rv + Cl * (rc + rr) + Ci * (ri + rs + rg)
