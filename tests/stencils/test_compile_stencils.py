0cbb981b72363afee61dd6d30abf85be3e79b3b8
Loic Maurin <maurin.loic.ac@gmail.com>
2024-01-09 16:58:00 +0100

    tests for stencil compilation

--- tests/stencils/test_compile_stencils.py
+++ tests/stencils/test_compile_stencils.py
@@ -1,4 +1,5 @@
-from ifs_physics_common.framework.stencil import compile_stencil 
+# -*- coding: utf-8 -*-
+from ifs_physics_common.framework.stencil import compile_stencil
 from phyex_gt4py.drivers.config import default_python_config
 import sys
 import logging
@@ -7,44 +8,60 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 if __name__ == "__main__":
-    
-    logging.info("Compile condensation stencil")
-    
+
+    logging.info("Compile ice_adjust stencil")
+
     externals = {
-        "lvtt":0,
-        "lstt":0,
-        "tt":0,
-        "subg_mf_pdf":0,
-        "subg_cond":0,
-        "cpd":0,
-        "cpv":0,
-        "Cl":0,
-        "Ci":0,
-        "tt":0,
-        "alpw":0,
-        "betaw":0,
-        "gamw":0,
-        "alpi":0,
-        "betai":0,
-        "gami":0,
-        "Rd":0,
-        "Rv":0,
-        "frac_ice_adjust":0,
-        "tmaxmix":0,
-        "tminmix":0,
-        "criautc":0,
-        "tstep":0,
-        "criauti":0,
-        "acriauti":0,
-        "bcriauti":0,
-        "nrr":6, 
+        "lvtt": 0,
+        "lstt": 0,
+        "tt": 0,
+        "subg_mf_pdf": 0,
+        "subg_cond": 0,
+        "cpd": 0,
+        "cpv": 0,
+        "Cl": 0,
+        "Ci": 0,
+        "tt": 0,
+        "alpw": 0,
+        "betaw": 0,
+        "gamw": 0,
+        "alpi": 0,
+        "betai": 0,
+        "gami": 0,
+        "Rd": 0,
+        "Rv": 0,
+        "frac_ice_adjust": 0,
+        "tmaxmix": 0,
+        "tminmix": 0,
+        "criautc": 0,
+        "tstep": 0,
+        "criauti": 0,
+        "acriauti": 0,
+        "bcriauti": 0,
+        "nrr": 6,
     }
 
     ice_adjust = compile_stencil(
-        "ice_adjust",
-        default_python_config.gt4py_config,
-        externals=externals
-        )
-    
+        "ice_adjust", default_python_config.gt4py_config, externals=externals
+    )
+
     logging.info("Compilation succeeded")
 
+    logging.info("Compile aro_adjust stencil")
+
+    externals = {
+        "lvtt": 0,
+        "lstt": 0,
+        "tt": 0,
+        "cpd": 0,
+        "cpv": 0,
+        "Cl": 0,
+        "Ci": 0,
+        "tt": 0,
+    }
+
+    aro_adjust = compile_stencil(
+        "aro_adjust", default_python_config.gt4py_config, externals=externals
+    )
+
+    logging.info("Compilation succeeded")
