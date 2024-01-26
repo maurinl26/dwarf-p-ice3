91a5ab12e2aaa51e63c558fbf546b2054c418463
Loic Maurin <maurin.loic.ac@gmail.com>
2024-01-26 16:04:45 +0100

    first version of statistical sedimentation

--- src/ice3_gt4py/stencils/sedimentation.py
+++ src/ice3_gt4py/stencils/sedimentation.py
@@ -36,7 +36,11 @@ def sedimentation_stat(
     sea_mask: Field["int"],         # Mask for sea PSEA
     town_fraction: Field["float"],  # Fraction of map which is town PTOWN
     wgt_lbc_tmp: Field["float"],    # LBC weighted by sea fraction
-    sed_tmp: Field["float"],        # sedimentation source ZSED 
+    c_sed_tmp: Field["float"],      # sedimentation source for cloud droplets ZSED
+    r_sed_tmp: Field["float"],      # sedimentation source for rain 
+    g_sed_tmp: Field["float"],      # sedimentation source for graupel
+    i_sed_tmp: Field["float"],      # sedimentation source for ice
+    s_sed_tmp: Field["float"],      # sedimentation source for snow    
     # cloud subroutine
     qp_tmp: Field["float"],
     wlbda_tmp: Field["float"],      # 
@@ -59,12 +63,15 @@ def sedimentation_stat(
     from __externals__ import (
         lsedic, 
         rholw,          # volumic lass of liquid water  
-        crtmin,         # cloud droplet rc min  
+        c_rtmin,         # cloud droplet rc min  
+        r_rtmin,
         lbexc,
         cc,
         dc,
         cexvt,
-        fsedc           # Warning : as to be externalized
+        fsedc,
+        fsedr, 
+        exsedr,
     )
     
     # Note Hail is omitted
@@ -86,11 +93,11 @@ def sedimentation_stat(
             # subroutine cloud in fortran
             # 1. ray, lbc, fsedc, conc3d
             
-            qp_tmp = sed_tmp[0, 0, 1] * dt__rho_dz_tmp[0, 0, 0]
-            if rc_in > crtmin or qp_tmp > crtmin:
+            qp_tmp = c_sed_tmp[0, 0, 1] * dt__rho_dz_tmp[0, 0, 0]
+            if rc_in > c_rtmin or qp_tmp > c_rtmin:
                 
-                if rc_in > crtmin:
-                    wsedw1_tmp = weighted_sedimentation_source(
+                if rc_in > c_rtmin:
+                    wsedw1_tmp = terminal_velocity(
                         rc_in,
                         tht,
                         pabst,
@@ -103,8 +110,8 @@ def sedimentation_stat(
                     wsedw1_tmp = 0
 
 
-                if qp_tmp > crtmin:
-                    wsedw2_tmp = weighted_sedimentation_source(
+                if qp_tmp > c_rtmin:
+                    wsedw2_tmp = terminal_velocity(
                         qp_tmp,
                         tht,
                         pabst,
@@ -134,20 +141,165 @@ def sedimentation_stat(
                     dz,
                     sed_tmp
                 )
-                    
-            # END SUBROUTINE
+        # end lsedic      
+        # END SUBROUTINE
             
         # 2.2 rain
+        # Other species            
+        qp_tmp[0, 0, 0] = r_sed_tmp[0, 0, 1] * dt__rho_dz_tmp[0, 0, 0]
+        if rr_in > r_rtmin or qp_tmp > r_rtmin:
+            if rr_in > r_rtmin:
+                wsedw1_tmp = other_species(
+                        fsedr, 
+                        exsedr,
+                        rr_in,
+                        rhodref
+                    )
+            else:
+                wsedw1_tmp = 0
+                    
+            if qp_tmp > r_rtmin:
+                wsedw2_tmp = other_species(
+                        fsedr,
+                        exsedr,
+                        qp_tmp,
+                        rhodref
+                    )
+            else:
+                wsedw2_tmp = 0
+                    
+        else:
+            wsedw1_tmp = 0
+            wsedw2_tmp = 0
+                
+        sed_tmp = weighted_sedimentation_flux_1(
+                wsedw1_tmp,
+                dz,
+                rhodref,
+                rc_in,
+                dt
+            )
             
+        if wsedw2_tmp != 0:
+            sed_tmp = sed_tmp + weighted_sedimentation_flux_2(
+                    wsedw2_tmp,
+                    dt,
+                    dz,
+                    sed_tmp
+                )
+                
+        # 2.3 ice
+        
+        
+        # 2.4 snow
+        qp_tmp[0, 0, 0] = r_sed_tmp[0, 0, 1] * dt__rho_dz_tmp[0, 0, 0]
+        if rr_in > r_rtmin or qp_tmp > r_rtmin:
+            if rr_in > r_rtmin:
+                wsedw1_tmp = other_species(
+                        fsedr, 
+                        exsedr,
+                        rr_in,
+                        rhodref
+                    )
+            else:
+                wsedw1_tmp = 0
+                    
+            if qp_tmp > r_rtmin:
+                wsedw2_tmp = other_species(
+                        fsedr,
+                        exsedr,
+                        qp_tmp,
+                        rhodref
+                    )
+            else:
+                wsedw2_tmp = 0
+                    
+        else:
+            wsedw1_tmp = 0
+            wsedw2_tmp = 0
+                
+        sed_tmp = weighted_sedimentation_flux_1(
+                wsedw1_tmp,
+                dz,
+                rhodref,
+                rc_in,
+                dt
+            )
+            
+        if wsedw2_tmp != 0:
+            sed_tmp = sed_tmp + weighted_sedimentation_flux_2(
+                    wsedw2_tmp,
+                    dt,
+                    dz,
+                    sed_tmp
+                )
+        
+        # 2.5 graupel
+        qp_tmp[0, 0, 0] = r_sed_tmp[0, 0, 1] * dt__rho_dz_tmp[0, 0, 0]
+        if rr_in > r_rtmin or qp_tmp > r_rtmin:
+            if rr_in > r_rtmin:
+                wsedw1_tmp = other_species(
+                        fsedr, 
+                        exsedr,
+                        rr_in,
+                        rhodref
+                    )
+            else:
+                wsedw1_tmp = 0
+                    
+            if qp_tmp > r_rtmin:
+                wsedw2_tmp = other_species(
+                        fsedr,
+                        exsedr,
+                        qp_tmp,
+                        rhodref
+                    )
+            else:
+                wsedw2_tmp = 0
+                    
+        else:
+            wsedw1_tmp = 0
+            wsedw2_tmp = 0
+                
+        sed_tmp = weighted_sedimentation_flux_1(
+                wsedw1_tmp,
+                dz,
+                rhodref,
+                rc_in,
+                dt
+            )
+            
+        if wsedw2_tmp != 0:
+            sed_tmp = sed_tmp + weighted_sedimentation_flux_2(
+                    wsedw2_tmp,
+                    dt,
+                    dz,
+                    sed_tmp
+                )
+                    
         
     # 3. Sources 
+    # Calcul des tendances
     with computation(PARALLEL), interval(...):
         
-    NotImplemented
+        rcs_tnd = rcs_tnd + dt__rho_dz_tmp * (c_sed_tmp[0, 0, 1] - c_sed_tmp[0, 0, 0]) / dt
+        ris_tnd = ris_tnd + dt__rho_dz_tmp * (i_sed_tmp[0, 0, 1] - i_sed_tmp[0, 0, 0]) / dt
+        rss_tnd = rss_tnd + dt__rho_dz_tmp * (s_sed_tmp[0, 0, 1] - s_sed_tmp[0, 0, 0]) / dt
+        rgs_tnd = rgs_tnd + dt__rho_dz_tmp * (g_sed_tmp[0, 0, 1] - g_sed_tmp[0, 0, 0]) / dt
+        rrs_tnd = rrs_tnd + dt__rho_dz_tmp * (r_sed_tmp[0, 0, 1] - r_sed_tmp[0, 0, 0]) / dt
     
-    
+    # Instantaneous fluxes
+    with computation(PARALLEL), interval(0, 1):    
+        
+        inst_rc_out = c_sed_tmp / rholw
+        inst_rr_out = r_sed_tmp / rholw
+        inst_ri_out = i_sed_tmp / rholw
+        inst_rs_out = s_sed_tmp / rholw
+        inst_rg_out = g_sed_tmp / rholw
+         
+
 @function
-def weighted_sedimentation_source(
+def terminal_velocity(
     content: Field["float"],
     tht: Field["float"],
     pabst: Field["float"],
@@ -171,6 +323,7 @@ def weighted_sedimentation_source(
     return wsedw1
 
 # FWSED1
+# P1 in Bouteloup paper
 @function
 def weighted_sedimentation_flux_1(
     wsedw: Field["float"],
@@ -183,6 +336,7 @@ def weighted_sedimentation_flux_1(
     return min(rhodref * dz * content_in / dt, wsedw * rhodref, content_in)
 
 #FWSED2
+#P2 in Bouteloup paper
 @function
 def weighted_sedimentation_flux_2(
     wsedw: Field["float"],
@@ -192,3 +346,21 @@ def weighted_sedimentation_flux_2(
 ):
     
     return max(0, 1 - dz / (dt * wsedw)) * wsedsup[0, 0, 1]
+
+
+@function
+def other_species(
+    fsed: "float",
+    exsed: "float",
+    content_in: Field["float"],
+    rhodref: Field["float"]
+):
+    
+    from __externals__  import cexvt
+    
+    wsedw = fsed * content_in * (exsed - 1) * rhodref ** (exsed - cexvt - 1)
+    
+    return wsedw
+
+
+     
