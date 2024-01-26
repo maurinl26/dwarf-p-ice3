4d8d453ad6342150698638750a100f60d53cc0b3
Loic Maurin <maurin.loic.ac@gmail.com>
2024-01-26 17:52:17 +0100

    sedimentation.py replaced by statistical_sedimentation.py

--- src/ice3_gt4py/stencils/statistical_sedimentation.py
+++ src/ice3_gt4py/stencils/statistical_sedimentation.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 
 from gt4py.cartesian.gtscript import Field, function
 from gt4py.cartesian.gtscript import exp, log, sqrt, floor, atan
-from phyex_gt4py.functions.compute_ice_frac import compute_frac_ice
-from phyex_gt4py.functions.src_1d import src_1d
-from phyex_gt4py.functions.temperature import update_temperature
+from ice3_gt4py.functions.compute_ice_frac import compute_frac_ice
+from ice3_gt4py.functions.src_1d import src_1d
+from ice3_gt4py.functions.temperature import update_temperature
 
 
-from phyex_gt4py.functions.ice_adjust import (
+from ice3_gt4py.functions.ice_adjust import (
     vaporisation_latent_heat,
     sublimation_latent_heat,
 )
@@ -315,7 +315,7 @@ def terminal_velocity(
         cexvt,
         dc
     )
-    wlbda_tmp = 6.6e-8 * (101325 / pabst[0, 0, 0])*(tht[0, 0, 0] / 293.15)
+    wlbda_tmp = 6.6e-8 * (101325 / pabst[0, 0, 0]) * (tht[0, 0, 0] / 293.15)
     wlbdc_tmp = (lbc * conc3d / (rhodref * content)) ** lbexc
     cc_tmp = cc * (1 + 1.26 * wlbda_tmp * wlbdc_tmp / ray)
     wsedw1 = rhodref ** (-cexvt) * wlbdc_tmp * (-dc) * cc_tmp * fsedc
---
+++ src/ice3_gt4py/stencils/upwind_sedimentation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from gt4py.cartesian.gtscript import Field, function
+from gt4py.cartesian.gtscript import exp, log, sqrt, floor, atan
+from ice3_gt4py.functions.compute_ice_frac import compute_frac_ice
+from ice3_gt4py.functions.src_1d import src_1d
+from ice3_gt4py.functions.temperature import update_temperature
+
+
+from ifs_physics_common.framework.stencil import stencil_collection
+
+# 
+@stencil_collection("upstream_sedimentation")
+def upstream_sedimentation():
+    
+    NotImplemented
