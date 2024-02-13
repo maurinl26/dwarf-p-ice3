ce8d6ffa3520246711dcbe15bda2b4d05348bcfb
Loic Maurin <maurin.loic.ac@gmail.com>
2024-02-13 14:21:51 +0100

    aro_adjust passed

--- src/phyex_gt4py/components/aro_adjust.py
+++ src/phyex_gt4py/components/aro_adjust.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 from functools import cached_property
 from itertools import repeat
 from typing import Dict
+import logging
 
 from ifs_physics_common.framework.grid import ComputationalGrid
 from ifs_physics_common.framework.config import GT4PyConfig
@@ -16,6 +17,15 @@ from phyex_gt4py.phyex_common.phyex import Phyex
 
 
 class AroAdjust(ImplicitTendencyComponent):
+    """Implicit Tendency Component calling sequentially
+    - aro_filter : negativity filters
+    - ice_adjust : saturation adjustment of temperature and mixing ratios
+
+    aro_filter stencil is aro_adjust.F90 in PHYEX, from l210 to l366
+    ice_adjust stencil is ice_adjust.F90 in PHYEX
+
+    """
+
     def __init__(
         self,
         computational_grid: ComputationalGrid,
@@ -42,6 +52,10 @@ class AroAdjust(ImplicitTendencyComponent):
             }
         )
 
+        # aro_filter stands for the parts before 'call ice_adjust' in aro_adjust.f90
+        self.aro_filter = self.compile_stencil("aro_filter", externals)
+
+        # ice_adjust stands for ice_adjust.f90
         self.ice_adjust = self.compile_stencil("ice_adjust", externals)
 
     @cached_property
@@ -65,6 +79,7 @@ class AroAdjust(ImplicitTendencyComponent):
                 "units": "",
             },  # convective mass flux liquid mixing ratio
             "f_ri_mf": {"grid": (I, J, K), "units": ""},
+            "f_tht": {"grid": (I, J, K), "units": ""},
         }
 
     @cached_property
@@ -74,6 +89,9 @@ class AroAdjust(ImplicitTendencyComponent):
             "f_rvs": {"grid": (I, J, K), "units": ""},  # PRS(1)
             "f_rcs": {"grid": (I, J, K), "units": ""},  # PRS(2)
             "f_ris": {"grid": (I, J, K), "units": ""},  # PRS(4)
+            "f_rrs": {"grid": (I, J, K), "units": ""},
+            "f_rss": {"grid": (I, J, K), "units": ""},
+            "f_rgs": {"grid": (I, J, K), "units": ""},
             "f_th": {"grid": (I, J, K), "units": ""},  # ZRS(0)
             "f_rv": {"grid": (I, J, K), "units": ""},  # ZRS(1)
             "f_rc": {"grid": (I, J, K), "units": ""},  # ZRS(2)
@@ -82,17 +100,11 @@ class AroAdjust(ImplicitTendencyComponent):
             "f_rs": {"grid": (I, J, K), "units": ""},  # ZRS(5)
             "f_rg": {"grid": (I, J, K), "units": ""},  # ZRS(6)
             "f_cldfr": {"grid": (I, J, K), "units": ""},
-            # "f_ice_cld_wgt": {"grid": (I, J, K), "units": ""},
         }
 
     @cached_property
     def _diagnostic_properties(self) -> PropertyDict:
         return {
-            # "f_icldfr": {"grid": (I, J, K), "units": ""},
-            # "f_wcldfr": {"grid": (I, J, K), "units": ""},
-            # "f_ssio": {"grid": (I, J, K), "units": ""},
-            # "f_ssiu": {"grid": (I, J, K), "units": ""},
-            # "f_srcs": {"grid": (I, J, K), "units": ""},
             "f_ifr": {"grid": (I, J, K), "units": ""},
             "f_hlc_hrc": {"grid": (I, J, K), "units": ""},
             "f_hlc_hcf": {"grid": (I, J, K), "units": ""},
@@ -117,7 +129,6 @@ class AroAdjust(ImplicitTendencyComponent):
             "sbar": {"grid": (I, J, K), "units": ""},
             "sigma": {"grid": (I, J, K), "units": ""},
             "q1": {"grid": (I, J, K), "units": ""},
-            # Missing fields
             "lv": {"grid": (I, J, K), "units": ""},
             "ls": {"grid": (I, J, K), "units": ""},
             "cph": {"grid": (I, J, K), "units": ""},
@@ -140,7 +151,7 @@ class AroAdjust(ImplicitTendencyComponent):
 
         with managed_temporary_storage(
             self.computational_grid,
-            *repeat(((I, J, K), "float"), 20),
+            *repeat(((I, J, K), "float"), 22),
             gt4py_config=self.gt4py_config,
         ) as (
             rt,
@@ -163,15 +174,70 @@ class AroAdjust(ImplicitTendencyComponent):
             ri_tmp,
             rc_tmp,
             t_tmp,
+            cor_tmp,
+            cph_tmp,
         ):
+
+            input_filter_keys = ["f_exnref", "f_tht"]
+
+            tendencies_filter_keys = [
+                "f_ths",
+                "f_rvs",
+                "f_rcs",
+                "f_ris",
+                "f_rrs",
+                "f_rss",
+                "f_rgs",
+            ]
+
+            logging.debug(f"State : {state.keys()}")
+            input_filter = {
+                name.split("_", maxsplit=1)[1]: state[name]
+                for name in input_filter_keys
+            }
+
+            logging.debug(f"Out tendencies : {out_tendencies.keys()}")
+            tendencies_filter = {
+                name.split("_", maxsplit=1)[1]: out_tendencies[name]
+                for name in tendencies_filter_keys
+            }
+
+            temporaries_filter = {
+                "cor_tmp": cor_tmp,  # ZCOR in aro_adjust.f90
+                "cph_tmp": cph_tmp,
+                "t_tmp": t_tmp,
+                "lv_tmp": lv,
+                "ls_tmp": ls,
+            }
+
+            logging.debug("Launching aro_filter")
+            self.aro_filter(
+                **input_filter,
+                **tendencies_filter,
+                # **diagnostics_filter,
+                **temporaries_filter,
+                dt=timestep.total_seconds(),
+                origin=(0, 0, 0),
+                domain=self.computational_grid.grids[I, J, K].shape,
+                validate_args=self.gt4py_config.validate_args,
+                exec_info=self.gt4py_config.exec_info,
+            )
+            logging.debug("aro_filter passed")
+
             inputs = {
                 name.split("_", maxsplit=1)[1]: state[name]
                 for name in self.input_properties
             }
+            inputs.pop("tht")
+
             tendencies = {
                 name.split("_", maxsplit=1)[1]: out_tendencies[name]
                 for name in self.tendency_properties
             }
+            tendencies.pop("rrs")
+            tendencies.pop("rss")
+            tendencies.pop("rgs")
+
             diagnostics = {
                 name.split("_", maxsplit=1)[1]: out_diagnostics[name]
                 for name in self.diagnostic_properties
