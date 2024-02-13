64b967bc5458cc2eac6ee5f04570c30161c075b7
Loic Maurin <maurin.loic.ac@gmail.com>
2024-02-13 13:51:23 +0100

    docs updated + install procedure

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 which python3
+
+echo "Updating PHYEX as a submodule"
+git submodule update --init --recursive
+
+echo "Creating environment"
+
 python3 -m venv .venv
 
+echo "Installing requirements"
 source .venv/bin/activate
 pip install -r requirements.txt
