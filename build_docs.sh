09b96a795ddff7582e67d918ff057b2bf10ae9d6
Loic Maurin <maurin.loic.ac@gmail.com>
2024-02-13 10:41:43 +0100

    run tests and build doc

---
+++ build_docs.sh
@@ -0,0 +1,5 @@
+
+source .venv/bin/activate
+export PYTHONPATH=$PYTHONPATH:$HOME/maurinl26/dwarf-ice3-gt4py/src
+
+python3 -m mkdocs serve
--- tests.sh
+++ tests.sh
@@ -8,7 +8,10 @@ python tests/stencils/test_compile_stencils.py
 echo "Stencil compilation completed"
 
 echo "Test components"
+echo "Test ice adjust"
 python tests/components/test_ice_adjust.py
+echo "Ice Adjust passed"
+echo "Test aro adjust"
+python tests/components/test_aro_adjust.py
+echo "Aro Adjust passed"
 echo "Components tests completed"
-
-
