17071b6e3259b825a14d96122a5c5e4523466d52
Loic Maurin <maurin.loic.ac@gmail.com>
2024-02-23 14:36:23 +0100

    ice4_rrhong with docstring and consistent naming of variables

--- src/ice3_gt4py/stencils/ice4_rrhong.py
+++ src/ice3_gt4py/stencils/ice4_rrhong.py
@@ -8,26 +8,41 @@ from ifs_physics_common.framework.stencil import stencil_collection
 
 @stencil_collection("ice4_rrhong")
 def ice4_rrhong(
+    ldcompute: Field["float"],
     t: Field["float"],
     exn: Field["float"],
     lv_fact: Field["float"],
     ls_fact: Field["float"],
     tht: Field["float"],  # theta at time t
-    rrhong_mr_out: Field["float"],
-    rr_in: Field["float"],  # rain water mixing ratio at t
-    ld_compute: Field["float"],  # mask of computation
+    rrhong_mr: Field["float"],
+    rr_t: Field["float"],  # rain water mixing ratio at t
 ):
+    """Compute the spontaneous frezzing source RRHONG
+
+    Args:
+        ldcompute (Field[float]): switch to activate microphysical processes on column
+        t (Field[float]): temperature at t
+        exn (Field[float]): exner pressure
+        lv_fact (Field[float]): vaporisation latent heat
+        ls_fact (Field[float]): sublimation latent heat
+        tht (Field[float]): potential temperature
+        rr_t (Field[float]): rain mixing ratio at t
+        rrhong_mr (Field[float]): mixing ratio for spontaneous freezing source
+    """
 
     from __externals__ import r_rtmin, tt, lfeedbackt
 
+    # 3.3 compute the spontaneous frezzing source: RRHONG
     with computation(PARALLEL), interval(...):
 
-        if t < tt - 35 and rr_in > r_rtmin and ldcompute == 1:
-            rrhong_mr_out = rr_in
+        if t < tt - 35 and rr_t > r_rtmin and ldcompute == 1:
+            rrhong_mr = rr_t
+
+            # limitation for -35 degrees crossing
             if lfeedbackt == 1:
-                rrhong_mr_out = min(
-                    rrhong_mr_out, max(0, ((tt - 35) / exn - tht) / (ls_fact - lv_fact))
+                rrhong_mr = min(
+                    rrhong_mr, max(0, ((tt - 35) / exn - tht) / (ls_fact - lv_fact))
                 )
 
         else:
-            rrhong_mr_out = 0
+            rrhong_mr = 0
