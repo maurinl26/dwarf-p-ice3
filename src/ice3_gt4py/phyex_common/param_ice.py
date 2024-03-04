32a39f029a40b54d9f53529908fafb56a39ea675
Loic Maurin <maurin.loic.ac@gmail.com>
2024-03-04 15:01:20 +0100

    param ice reworked with enums to handle options

--- src/ice3_gt4py/phyex_common/param_ice.py
+++ src/ice3_gt4py/phyex_common/param_ice.py
@@ -7,7 +7,8 @@ from enum import Enum
 
 from ice3_gt4py.drivers.namel2config import Namparar
 
-
+# Stands for CSUBG_MF_PDF in modd_param_icen.F90
+# Default to NONE
 class SubGridMassFluxPDF(Enum):
 
     NONE = 0
@@ -22,11 +23,44 @@ class SubgRRRCAccr(Enum):
     PRFR = 1
 
 
+# Stands for CSUBG_RR_EVAP in modd_param_icen.F90
+# Default to NONE
 class SubgRREvap(Enum):
 
     NONE = 0
     CLFR = 1
     PRFR = 2
+
+
+# Stands for CSUBG_PR_PDF in modd_param_icen.F90
+# Default to SIGM
+class SubgPRPDF(Enum):
+
+    SIGM = 0
+    HLCRECTPDF = 1
+    HLCISOTRIPDF = 2
+    HLCTRIANGPDF = 3
+    HLCQUADRAPDF = 4
+
+
+# Stands for CSUBG_AUCV_RC in modd_param_icen.F90
+# Default to NONE
+class SubgAucvRc(Enum):
+
+    NONE = 0
+    PDF = 1
+    ADJU = 2
+    CLFR = 3
+    SIGM = 4
+
+
+# Stands for CSUBG_AUCV_RI in modd_param_icen.F90
+# Default to NONE
+class SubgAucvRi(Enum):
+
+    NONE = 0
+    CLFR = 1
+    ADJU = 2
 
 
 @ported_class(from_file="PHYEX/src/common/aux/modd_param_icen.F90")
@@ -51,7 +85,7 @@ class ParamIce:
 
     subg_rc_rr_accr: str    # subgrid rc-rr accretion
     subg_rr_evap: str       # subgrid rr evaporation
-    subg_rr_pdf: str        # pdf for subgrid precipitation
+    subg_pr_pdf: str        # pdf for subgrid precipitation
     subg_aucv_rc: str       # type of subgrid rc->rr autoconv. method
     subg_aucv_ri: str       # type of subgrid ri->rs autoconv. method
     subg_mf_pdf: str        # PDF to use for MF cloud autoconversions
@@ -90,8 +124,12 @@ class ParamIce:
 
     vdeposc: float = field(default=0.02)  # Droplet deposition velocity
 
-    pristine_ice: str = field(default="PLAT")  # Pristine ice type PLAT, COLU, or BURO
-    sedim: str = field(default="SPLI")  # Sedimentation calculation mode
+    pristine_ice: Literal["PLAT", "COLU", "BURO"] = field(
+        default="PLAT"
+    )  # Pristine ice type PLAT, COLU, or BURO
+    sedim: Literal["SPLI", "STAT", "NONE"] = field(
+        default="SPLI"
+    )  # Sedimentation calculation mode
 
     # To use modified ice3/ice4 - to reduce time step dependency
     lred: bool = field(default=True)
@@ -100,9 +138,15 @@ class ParamIce:
         default=SubgRRRCAccr.NONE.value
     )  # subgrid rc-rr accretion
     subg_rr_evap: int = field(default=SubgRREvap.NONE.value)  # subgrid rr evaporation
-    subg_rr_pdf: str = field(default="SIGM")  # pdf for subgrid precipitation
-    subg_aucv_rc: str = field(default="NONE")  # type of subgrid rc->rr autoconv. method
-    subg_aucv_ri: str = field(default="NONE")  # type of subgrid ri->rs autoconv. method
+    subg_pr_pdf: int = field(
+        default=SubgPRPDF.SIGM.value
+    )  # pdf for subgrid precipitation
+    subg_aucv_rc: int = field(
+        default=SubgAucvRc.NONE.value
+    )  # type of subgrid rc->rr autoconv. method
+    subg_aucv_ri: int = field(
+        default=SubgAucvRi.NONE.value
+    )  # type of subgrid ri->rs autoconv. method
 
     # PDF to use for MF cloud autoconversions
     subg_mf_pdf: int = field(default=SubGridMassFluxPDF.TRIANGLE.value)
@@ -144,24 +188,7 @@ class ParamIce:
 
     def __post_init__(self):
         self.t0criauti_nam = (np.log10(self.criauti_nam) - self.brcriauti_nam) / 0.06
-
-        self.frmin_nam = np.empty(41)
-        self.frmin_nam[1:6] = 0
-        self.frmin_nam[7:9] = 1.0
-        self.frmin_nam[10] = 10.0
-        self.frmin_nam[11] = 1.0
-        self.frmin_nam[12] = 0.0
-        self.frmin_nam[13] = 1.0e-15
-        self.frmin_nam[14] = 120.0
-        self.frmin_nam[15] = 1.0e-4
-        self.frmin_nam[16:20] = 0.0
-        self.frmin_nam[21:22] = 1.0
-        self.frmin_nam[23] = 0.5
-        self.frmin_nam[24] = 1.5
-        self.frmin_nam[25] = 30.0
-        self.frmin_nam[26:38] = 0.0
-        self.frmin_nam[39] = 0.25
-        self.frmin_nam[40] = 0.15
+        self.set_frmin_nam()
 
         if self.hprogram == "AROME":
             self.lconvhg = True
@@ -183,3 +210,25 @@ class ParamIce:
             self.lconvhg = True
             self.ladj_before = True
             self.ladj_after = True
+
+    def set_frmin_nam(self):
+
+        tmp_frmin_nam = np.empty(41)
+        tmp_frmin_nam[1:6] = 0
+        tmp_frmin_nam[7:9] = 1.0
+        tmp_frmin_nam[10] = 10.0
+        tmp_frmin_nam[11] = 1.0
+        tmp_frmin_nam[12] = 0.0
+        tmp_frmin_nam[13] = 1.0e-15
+        tmp_frmin_nam[14] = 120.0
+        tmp_frmin_nam[15] = 1.0e-4
+        tmp_frmin_nam[16:20] = 0.0
+        tmp_frmin_nam[21:22] = 1.0
+        tmp_frmin_nam[23] = 0.5
+        tmp_frmin_nam[24] = 1.5
+        tmp_frmin_nam[25] = 30.0
+        tmp_frmin_nam[26:38] = 0.0
+        tmp_frmin_nam[39] = 0.25
+        tmp_frmin_nam[40] = 0.15
+
+        self.frmin_nam = tmp_frmin_nam
