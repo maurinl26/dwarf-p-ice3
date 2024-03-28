96e4799c33eb51f08d289de2f0ff2b61194dc2c2
Loic Maurin <maurin.loic.ac@gmail.com>
2024-03-28 10:19:48 +0100

    unused imports removed

--- src/ice3_gt4py/stencils/statistical_sedimentation.py
+++ src/ice3_gt4py/stencils/statistical_sedimentation.py
@@ -2,21 +2,12 @@
 from __future__ import annotations
 
 from gt4py.cartesian.gtscript import Field, function
-from gt4py.cartesian.gtscript import exp, log, sqrt, floor, atan
-from ice3_gt4py.functions.compute_ice_frac import compute_frac_ice
 from ice3_gt4py.functions.sedimentation_flux import (
     other_species,
     weighted_sedimentation_flux_1,
     weighted_sedimentation_flux_2,
 )
-from ice3_gt4py.functions.src_1d import src_1d
-from ice3_gt4py.functions.temperature import update_temperature
 
-
-from ice3_gt4py.functions.ice_adjust import (
-    vaporisation_latent_heat,
-    sublimation_latent_heat,
-)
 from ifs_physics_common.framework.stencil import stencil_collection
 
 
