75de11b09e871f9bc5b7aeb2f25264ec0ecb7cb9
Loic Maurin <maurin.loic.ac@gmail.com>
2024-04-02 12:00:50 +0200

    saturation functions

--- src/ice3_gt4py/functions/tiwmx.py
+++ src/ice3_gt4py/functions/tiwmx.py
@@ -7,19 +7,41 @@ from ice3_gt4py.functions.sign import sign
 
 
 @function
-def esatw(alpw: float, betaw: float, tt: Field["float"]):
-    
+def e_sat_w(t: Field["float"]) -> Field["float"]:
+    """Saturation vapor pressure over liquid water
+
+    Args:
+        t (Field[float]): temperature
+
+    Returns:
+        Field[float]: saturation vapor pressure
+    """
+
     from __externals__ import ALPW, BETAW, GAMW
 
-    return exp(ALPW - BETAW / tt[0, 0, 0] - GAMW * log(tt[0, 0, 0]))
+    return exp(ALPW - BETAW / t - GAMW * log(t))
 
 
 @function
-def esati(cst_tt: float, alpw: float, betaw: float, tt: Field["float"]):
-    
-    from __externals__ import ALPW, BETAW, GAMW
-    
-    return (0.5 + sign(0.5, tt - cst_tt)) * esatw(tt) - (
-        sign(0.5, tt - cst_tt) - 0.5
-    ) * esatw(tt)
+def e_sat_i(t: Field["float"]):
+    """Saturation vapor pressure over ice
 
+    Args:
+        t (Field[float]): temperature
+
+    Returns:
+        Field[float]: saturation vapor pressure
+    """
+    from __externals__ import ALPI, BETAI, GAMI
+
+    return exp(ALPI - BETAI / t - GAMI * log(t))
+
+
+# @function
+# def esati(cst_tt: float, alpw: float, betaw: float, tt: Field["float"]):
+
+#     from __externals__ import ALPW, BETAW, GAMW
+
+#     return (0.5 + sign(0.5, tt - cst_tt)) * esatw(tt) - (
+#         sign(0.5, tt - cst_tt) - 0.5
+#     ) * esatw(tt)
