66e5325cde572a12daf29741d9ebc01cb3024807
Loic Maurin <maurin.loic.ac@gmail.com>
2024-04-23 12:42:25 +0200

    cleaning imports

--- src/ice3_gt4py/stencils/ice_adjust.py
+++ src/ice3_gt4py/stencils/ice_adjust.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from gt4py.cartesian.gtscript import (PARALLEL, Field, GlobalTable, atan,
-                                      computation, exp, floor, interval, log,
-                                      sqrt)
+from gt4py.cartesian.gtscript import (
+    PARALLEL,
+    Field,
+    GlobalTable,
+    atan,
+    computation,
+    exp,
+    floor,
+    interval,
+    log,
+    sqrt,
+)
 from ifs_physics_common.framework.stencil import stencil_collection
 import numpy as np
 
 from ice3_gt4py.functions.compute_ice_frac import compute_frac_ice
-from ice3_gt4py.functions.ice_adjust import (sublimation_latent_heat,
-                                             vaporisation_latent_heat)
+from ice3_gt4py.functions.ice_adjust import (
+    sublimation_latent_heat,
+    vaporisation_latent_heat,
+)
 from ice3_gt4py.functions.temperature import update_temperature
 from ice3_gt4py.functions.tiwmx import e_sat_i, e_sat_w
-# from ice3_gt4py.drivers.config import externals
 
 # TODO: remove POUT (not used in aro_adjust)
 # TODO: add SSIO, SSIU, IFR, SRCS
@@ -70,48 +80,61 @@ def ice_adjust(
 ):
     """Microphysical adjustments for specific contents due to condensation.
 
-        Args:
-            sigqsat (Field[float]): _description_
-            exnref (Field[float]): reference exner pressure
-            exn (Field[float]): true exner pressure
-            rhodref (Field[float]): reference density
-            pabs (Field[float]): absolute pressure at time t
-            sigs (Field[float]): _description_
-            cf_mf (Field[float]): convective mass flux cloud fraction             (from shallow convection)
-            rc_mf (Field[float]): convective mass flux liquid mixing ratio  (from shallow convection)
-            ri_mf (Field[float]): convective mass flux ice mixing ratio     (from shallow convection)
-            th (Field[float]): potential temperature
-            rv (Field[float]): water vapour m.r. to adjust
-            rc (Field[float]): cloud water m.r. to adjust
-            ri (Field[float]): cloud ice m.r. to adjust
-            rr (Field[float]): rain water m.r. to adjust
-            rs (Field[float]): snow m.r. to adjust
-            rg (Field[float]): graupel m.r. to adjust
-            ths (Field[float]): potential temperature source
-            rvs (Field[float]): water vapour source
-            rcs (Field[float]): cloud droplets source
-            ris (Field[float]): ice source
-            cldfr (Field[float]): cloud fraction
-            ifr (Field[float]): ratio cloud ice moist part to dry part
-            hlc_hrc (Field[float]): _description_
-            hlc_hcf (Field[float]): _description_
-            hli_hri (Field[float]): _description_
-            hli_hcf (Field[float]): _description_
-            sigrc (Field[float]): _description_
-            rv_tmp (Field[float]): _description_
-            ri_tmp (Field[float]): _description_
-            rc_tmp (Field[float]): _description_
-            t_tmp (Field[float]): _description_
-            cph (Field[float]): _description_
-            frac_tmp (Field[float]): _description_
-            sigma (Field[float]): _description_
-            q1 (Field[float]): _description_
-            dt (float): _description_
+    Args:
+        sigqsat (Field[float]): _description_
+        exnref (Field[float]): reference exner pressure
+        exn (Field[float]): true exner pressure
+        rhodref (Field[float]): reference density
+        pabs (Field[float]): absolute pressure at time t
+        sigs (Field[float]): _description_
+        cf_mf (Field[float]): convective mass flux cloud fraction             (from shallow convection)
+        rc_mf (Field[float]): convective mass flux liquid mixing ratio  (from shallow convection)
+        ri_mf (Field[float]): convective mass flux ice mixing ratio     (from shallow convection)
+        th (Field[float]): potential temperature
+        rv (Field[float]): water vapour m.r. to adjust
+        rc (Field[float]): cloud water m.r. to adjust
+        ri (Field[float]): cloud ice m.r. to adjust
+        rr (Field[float]): rain water m.r. to adjust
+        rs (Field[float]): snow m.r. to adjust
+        rg (Field[float]): graupel m.r. to adjust
+        ths (Field[float]): potential temperature source
+        rvs (Field[float]): water vapour source
+        rcs (Field[float]): cloud droplets source
+        ris (Field[float]): ice source
+        cldfr (Field[float]): cloud fraction
+        ifr (Field[float]): ratio cloud ice moist part to dry part
+        hlc_hrc (Field[float]): _description_
+        hlc_hcf (Field[float]): _description_
+        hli_hri (Field[float]): _description_
+        hli_hcf (Field[float]): _description_
+        sigrc (Field[float]): _description_
+        rv_tmp (Field[float]): _description_
+        ri_tmp (Field[float]): _description_
+        rc_tmp (Field[float]): _description_
+        t_tmp (Field[float]): _description_
+        cph (Field[float]): _description_
+        frac_tmp (Field[float]): _description_
+        sigma (Field[float]): _description_
+        q1 (Field[float]): _description_
+        dt (float): _description_
     """
 
-    from __externals__ import (ACRIAUTI, BCRIAUTI,
-                               CI, CL, CPD, CPV, CRIAUTC, CRIAUTI,
-                               NRR, RD, RV, SUBG_COND, SUBG_MF_PDF, TT)
+    from __externals__ import (
+        ACRIAUTI,
+        BCRIAUTI,
+        CI,
+        CL,
+        CPD,
+        CPV,
+        CRIAUTC,
+        CRIAUTI,
+        NRR,
+        RD,
+        RV,
+        SUBG_COND,
+        SUBG_MF_PDF,
+        TT,
+    )
 
     # 2.3 Compute the variation of mixing ratio
     with computation(PARALLEL), interval(...):
@@ -184,9 +207,7 @@ def ice_adjust(
 
         if rc_tmp > ri_tmp:
             if ri_tmp > 1e-20:
-                frac_tmp = ri_tmp / (
-                    rc_tmp + ri_tmp
-                )
+                frac_tmp = ri_tmp / (rc_tmp + ri_tmp)
 
         frac_tmp = compute_frac_ice(t_tmp)
 
@@ -198,12 +219,10 @@ def ice_adjust(
         lvs = (1 - frac_tmp) * lv + frac_tmp * ls
 
         # # coefficients a et b
-        ah = lvs * qsl / (RV * t_tmp ** 2) * (1 + RV * qsl / RD)
+        ah = lvs * qsl / (RV * t_tmp**2) * (1 + RV * qsl / RD)
         a = 1 / (1 + lvs / cph * ah)
         # # b = ah * a
-        sbar = a * (
-            rt - qsl + ah * lvs * (rc_tmp + ri_tmp * prifact) / CPD
-        )
+        sbar = a * (rt - qsl + ah * lvs * (rc_tmp + ri_tmp * prifact) / CPD)
 
         sigma = sqrt((2 * sigs) ** 2 + (sigqsat * qsl * a) ** 2)
         sigma = max(1e-10, sigma)
@@ -214,10 +233,10 @@ def ice_adjust(
         if q1 > 0:
             if q1 <= 2:
                 cond_tmp = min(
-                    exp(-1) + 0.66 * q1 + 0.086 * q1 ** 2, 2
+                    exp(-1) + 0.66 * q1 + 0.086 * q1**2, 2
                 )  # we use the MIN function for continuity
         elif q1 > 2:
-            cond_tmp= q1
+            cond_tmp = q1
         else:
             cond_tmp = exp(1.2 * q1 - 1)
 
@@ -228,22 +247,18 @@ def ice_adjust(
 
         cond_tmp = 0 if cldfr == 0 else cond_tmp
 
-        inq1 = floor(min(
-            10, max(-22, min(-100, 2 * floor(q1))) 
-        )) # inner min/max prevents sigfpe when 2*zq1 does not fit dtype_into an "int"
-        
+        inq1 = floor(
+            min(10, max(-22, min(-100, 2 * floor(q1))))
+        )  # inner min/max prevents sigfpe when 2*zq1 does not fit dtype_into an "int"
+
         inc = 2 * q1 - inq1
-                                
-        sigrc = min(
-            1, (1 - inc) * src_1d.A[inq1] + inc * src_1d.A[inq1 + 1]
-        )
+
+        sigrc = min(1, (1 - inc) * src_1d.A[inq1] + inc * src_1d.A[inq1 + 1])
 
         # # Translation notes : 506 -> 514 (not ocnd2)
         rc_tmp = (1 - frac_tmp) * cond_tmp  # liquid condensate
-        ri_tmp = frac_tmp * cond_tmp        # solid condensate
-        t_tmp = update_temperature(
-            t_tmp, rc_tmp, rc_tmp, ri_tmp, ri_tmp, lv, ls
-        )
+        ri_tmp = frac_tmp * cond_tmp  # solid condensate
+        t_tmp = update_temperature(t_tmp, rc_tmp, rc_tmp, ri_tmp, ri_tmp, lv, ls)
         rv_tmp = rt - rc_tmp - ri_tmp * prifact
 
         # Transaltion notes : 566 -> 578 HLAMBDA3 = CB
@@ -285,9 +300,7 @@ def ice_adjust(
             rcs += w1
             ris += w2
             rvs -= w1 + w2
-            ths += (
-                (w1 * lv + w2 * ls) / cph / exnref
-            )
+            ths += (w1 * lv + w2 * ls) / cph / exnref
 
     # Droplets subgrid autoconversion
     with computation(PARALLEL), interval(...):
@@ -301,9 +314,7 @@ def ice_adjust(
         elif SUBG_MF_PDF == 1:
             if w1 * dt > cf_mf * criaut:
                 hcf = 1 - 0.5 * (criaut * cf_mf) / max(1e-20, w1 * dt)
-                hr = w1 * dt - (criaut * cf_mf) ** 3 / (
-                    3 * max(1e-20, w1 * dt) ** 2
-                )
+                hr = w1 * dt - (criaut * cf_mf) ** 3 / (3 * max(1e-20, w1 * dt) ** 2)
 
             elif 2 * w1 * dt <= cf_mf * criaut:
                 hcf = 0
@@ -338,18 +349,14 @@ def ice_adjust(
         elif SUBG_MF_PDF == 1:
             if w2 * dt > cf_mf * criaut:
                 hli_hcf = 1 - 0.5 * ((criaut * cf_mf) / (w2 * dt)) ** 2
-                hli_hri = w2 * dt - (criaut * cf_mf) ** 3 / (
-                    3 * (w2 * dt) ** 2
-                )
+                hli_hri = w2 * dt - (criaut * cf_mf) ** 3 / (3 * (w2 * dt) ** 2)
 
         elif 2 * w2 * dt <= cf_mf * criaut:
             hli_hcf = 0
             hli_hri = 0
 
         else:
-            hli_hcf = (2 * w2 * dt - criaut * cf_mf) ** 2 / (
-                2.0 * (w2 * dt) ** 2
-            )
+            hli_hcf = (2 * w2 * dt - criaut * cf_mf) ** 2 / (2.0 * (w2 * dt) ** 2)
             hli_hri = (
                 4.0 * (w2 * dt) ** 3
                 - 3.0 * w2 * dt * (criaut * cf_mf) ** 2
