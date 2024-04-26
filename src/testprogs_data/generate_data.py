b362b3bf27efde676a0307dfc3cb9c16f205a38f
Loic Maurin <maurin.loic.ac@gmail.com>
2024-04-26 17:35:44 +0200

    script for data extraction

--- src/testprogs_data/generate_data.py
+++ src/testprogs_data/generate_data.py
@@ -4,134 +4,28 @@ import logging
 import sys
 import os
 import xarray as xr
-import typer
+from pathlib import Path
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 logging.getLogger()
 
 ################### READ FORTRAN FILE #####################
-def get_array(count):
+def get_array(f, count):
     n_memory = np.fromfile(f, dtype=">i4", count=1)
-    logging.info(f"Memory {n_memory}")
-    array = np.fromfile(f, dtype=">f8", count=count)
-    _ = np.fromfile(f, dtype=">i4", count=1)
+    if n_memory != 0:
+        logging.info(f"Memory {n_memory}")
+        array = np.fromfile(f, dtype=">f8", count=count)
+        _ = np.fromfile(f, dtype=">i4", count=1)
+    else:
+        array = np.empty()
 
     return array
 
 
-def get_dims():
+def get_dims(f):
     dims = np.fromfile(f, dtype=">i4", count=1)
     logging.info(f"Dims={dims}")
     KLON, KDUM, KLEV = np.fromfile(f, dtype=">i4", count=3)
     _ = np.fromfile(f, dtype=">i4", count=1)
 
     return KLON, KDUM, KLEV
-
-
-if __name__ == "__main__":
-
-    # in main_ice_adjust.F90
-    NPROMA = 32
-    NGPBLKS = 296
-    NGPTOT = NPROMA * NGPBLKS
-
-    KRR = 6  # Number of species
-
-    # in getdata_ice_adjust.F90
-    IBL = 1  # File number
-    FILE_PATH = f"/home/maurinl/PHYEX/tools/testprogs_data/ice_adjust/{IBL:08}.dat"
-    OUTPUT_PATH = "/home/maurinl/PHYEX/tools/testprogs_data/ice_adjust/reference.nc"
-
-    FIELD_KEYS = [
-        "PRHODJ",
-        "PEXNREF",
-        "PRHODREF",
-        "PPABSM",
-        "PTHT",
-        "PSIGS",
-        "PMFCONV",
-        "PRC_MF",
-        "PRI_MF",
-        "PCF_MF",
-        "PTHS",
-        "PRS",  # decode_array(KLON*KLEV*KRR)
-        # "PRS_OUT",      # decode_array(KLON*KLEV*KRR)
-        # "PSRCS_OUT",
-        # "PCLDFR_OUT",
-        # "ZRS",          # decode_array(KLON*KLEV*(KRR+1))
-        # "ZZZ",
-        # "PHLC_HRC_OUT",
-        # "PHLC_HCF_OUT",
-        # "PHLI_HRI_OUT",
-        # "PHLI_HCF_OUT",
-    ]
-
-    output_dataset = xr.Dataset()
-
-    ###### Loop over files ########
-    # Slicing
-    IOFF = 0
-    while os.path.isfile(FILE_PATH):
-
-        logging.info(f"IBL : {IBL}")
-        logging.info(f"Decoding : {FILE_PATH}")
-        ##### Processing file.dat #####
-        with open(FILE_PATH, "r") as f:
-
-            #  READ (IFILE) KLON, KDUM, KLEV
-            KLON, KDUM, KLEV = get_dims()
-            logging.info(f"KLON={KLON}, KLEV={KLEV}, KDUM={KDUM}")
-
-            for key in FIELD_KEYS:
-                logging.info(f"Decoding : {key}")
-
-                if key in ["PRS", "PRS_OUT"]:
-                    data_array = xr.DataArray(
-                        data=get_array(KLON * KLEV * KRR).reshape(
-                            (KLON, KLEV, KRR), order="F"
-                        ),
-                        dims=["IJ", "K", "Specy"],
-                        coords={
-                            "IJ": range(IOFF + 1, IOFF + KLON + 1),
-                            "K": KLEV,
-                            "Specy": ["v", "c", "r", "i", "s", "g"],
-                        },
-                        name=key,
-                    )
-
-                if key in ["ZRS"]:
-                    data_array = xr.DataArray(
-                        data=get_array(KLON * KLEV * (KRR + 1)).reshape(
-                            (KLON, KLEV, KRR + 1), order="F"
-                        ),
-                        dims=["IJ", "K", "Specy"],
-                        coords={
-                            "IJ": range(IOFF + 1, IOFF + KLON + 1),
-                            "K": KLEV,
-                            "Specy": ["th", "v", "c", "r", "i", "s", "g"],
-                        },
-                        name=key,
-                    )
-
-                else:
-                    data_array = xr.DataArray(
-                        data=get_array(KLON * KLEV).reshape((KLON, KLEV), order="F"),
-                        dims=["IJ", "K"],
-                        coords={
-                            "IJ": range(IOFF + 1, IOFF + KLON + 1),
-                            "K": range(0, KLEV),
-                        },
-                        name=key,
-                    )
-
-                output_dataset.update({f"{key}": data_array})
-
-            IBL += 1
-            FILE_PATH = (
-                f"/home/maurinl/PHYEX/tools/testprogs_data/ice_adjust/{IBL:08}.dat"
-            )
-
-            IOFF += KLON
-
-    # Output
-    output_dataset.to_netcdf(OUTPUT_PATH, format="NETCDF4", engine="netcdf4")
--- src/testprogs_data/main.py
+++ src/testprogs_data/main.py
@@ -5,6 +5,9 @@ import sys
 import os
 import xarray as xr
 import typer
+from pathlib import Path
+
+from testprogs_data.generate_data import get_array, get_dims
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 logging.getLogger()
@@ -13,10 +16,118 @@ logging.getLogger()
 app = typer.Typer()
 
 
+@app.command()
 def extract_testsprogs_data(
-    inputdir: str, output_file: str, nproma: int = 23, ngpblks: int = 296
+    dir: str,
+    output_file: str,
+    nproma: int = 23,
+    ngpblks: int = 296,
 ):
-    NotImplemented
+
+    KRR = 6
+
+    # in getdata_ice_adjust.F90
+    ibl = 0  # File number
+    file_path = Path(dir, f"{ibl:08}.dat")
+    output_path = Path(output_file)
+
+    FIELD_KEYS_LIST = [
+        "PRHODJ",
+        "PEXNREF",
+        "PRHODREF",
+        "PSIGS",
+        "PMFCONV",
+        "PPABSM",
+        "ZZZ",
+        "PCF_MF",
+        "PRC_MF",
+        "PRI_MF",
+        "ZRS",
+        "PRS",
+        "PTHS",
+        "PRS_OUT",
+        "PSRCS_OUT",
+        "PCLDFR_OUT",
+        "PHLC_HRC_OUT",
+        "PHLC_HCF_OUT",
+        "PHLI_HRI_OUT",
+        "PHLI_HCF_OUT",
+    ]
+
+    output_dataset = xr.Dataset()
+
+    ###### Loop over files ########
+    # Slicing
+    IOFF = 0
+    while file_path.is_file():
+
+        logging.info(f"IBL : {ibl}")
+        logging.info(f"Decoding : {file_path}")
+        ##### Processing file.dat #####
+        with open(file_path, "r") as f:
+
+            #  READ (IFILE) KLON, KDUM, KLEV
+            KLON, KDUM, KLEV = get_dims(f)
+            logging.info(f"KLON={KLON}, KLEV={KLEV}, KDUM={KDUM}")
+
+            file_dataset = xr.Dataset()
+
+            for key in FIELD_KEYS_LIST:
+                logging.info(f"Decoding : {key}")
+
+                if key in ["PRS", "PRS_OUT"]:
+                    data_array = xr.DataArray(
+                        data=get_array(f, KLON * KLEV * KRR).reshape(
+                            (KLON, KLEV, KRR), order="F"
+                        ),
+                        dims=["IJ", "K", "Specy"],
+                        coords={
+                            "IJ": range(IOFF + 1, IOFF + KLON + 1),
+                            "K": range(0, KLEV),
+                            "Specy": ["v", "c", "r", "i", "s", "g"],
+                        },
+                        name=f"{key}",
+                    )
+
+                elif key in ["ZRS"]:
+                    data_array = xr.DataArray(
+                        data=get_array(f, KLON * KLEV * (KRR + 1)).reshape(
+                            (KLON, KLEV, KRR + 1), order="F"
+                        ),
+                        dims=["IJ", "K", "Specy"],
+                        coords={
+                            "IJ": range(IOFF + 1, IOFF + KLON + 1),
+                            "K": range(0, KLEV),
+                            "Specy": ["th", "v", "c", "r", "i", "s", "g"],
+                        },
+                        name=f"{key}",
+                    )
+
+                elif key not in ["PRS", "PRS_OUT", "ZRS"]:
+                    data_array = xr.DataArray(
+                        data=get_array(f, KLON * KLEV).reshape((KLON, KLEV), order="F"),
+                        dims=["IJ", "K"],
+                        coords={
+                            "IJ": range(IOFF + 1, IOFF + KLON + 1),
+                            "K": range(0, KLEV),
+                        },
+                        name=f"{key}",
+                    )
+
+                file_dataset[key] = data_array
+
+                if ibl == 0:
+                    output_dataset = file_dataset
+                else:
+                    output_dataset = xr.merge([output_dataset, file_dataset])
+
+            ibl += 1
+            file_path = Path(dir, f"{ibl:08}.dat")
+
+            IOFF += KLON
+
+    # Output
+    output_dataset.to_netcdf(output_path, format="NETCDF4", engine="netcdf4")
 
 
 if __name__ == "__main__":
