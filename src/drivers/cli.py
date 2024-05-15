bd8d472345f2d6640e6695b6fe281531db68271a
Loic Maurin <maurin.loic.ac@gmail.com>
2024-05-15 13:34:47 +0200

    driver + cli refactoring

--- src/drivers/cli.py
+++ src/drivers/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 from pathlib import Path
+import subprocess
 import typer
 import logging
 import datetime
@@ -11,6 +12,7 @@ import xarray as xr
 from ifs_physics_common.framework.config import GT4PyConfig
 from ifs_physics_common.framework.grid import ComputationalGrid
 
+from drivers.core import core
 from ice3_gt4py.components.aro_adjust import AroAdjust
 from ice3_gt4py.components.ice4_tendencies import Ice4Tendencies
 from ice3_gt4py.components.ice_adjust import IceAdjust
@@ -22,9 +24,7 @@ from ice3_gt4py.initialisation.state_ice4_tendencies import (
     get_constant_state_ice4_tendencies,
 )
 from ice3_gt4py.initialisation.state_ice_adjust import (
-    get_constant_state_ice_adjust,
     get_state_ice_adjust,
-    ice_adjust_fields_keys,
 )
 from ice3_gt4py.phyex_common.phyex import Phyex
 from ice3_gt4py.utils.reader import NetCDFReader
@@ -33,6 +33,36 @@ logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 logging.getLogger()
 
 app = typer.Typer()
+
+
+@app.command()
+def run_ice_adjust_fortran(
+    testdir: str, name: str, archfile: str, checkOpt: str, extrapolation_opts: str
+):
+    """Call and run main_ice_adjust (Fortran)"""
+
+    try:
+
+        logging.info("Setting env variables")
+        subprocess.run(
+            ["source", f"{testdir}/{name}/build/with_fcm/arch_{archfile}/arch.env"]
+        )
+
+        logging.info("Job submit")
+        subprocess.run(
+            [
+                "submit",
+                "Output_run",
+                "Stderr_run",
+                f"{testdir}/{name}/build/with_fcm/arch_${archfile}/build/bin/main_ice_adjust.exe",
+                f"{checkOpt}",
+                f"{extrapolation_opts}",
+            ]
+        )
+
+    except RuntimeError as e:
+        logging.error("Fortran ice_adjust execution failed")
+        logging.error(f"{e}")
 
 
 @app.command()
@@ -108,7 +138,13 @@ def run_ice_adjust(backend: str, dataset: str, output_path: str, tracking_file: 
 def run_tendencies(backend: str, dataset: str, output_path: str, tracking_file: str):
     """Run ice4_tendencies_component on dummy data (one arrays)"""
 
-    ##### Grid #####
+    ######## Backend and gt4py config #######
+    logging.info(f"With backend {backend}")
+    gt4py_config = GT4PyConfig(
+        backend=backend, rebuild=True, validate_args=False, verbose=True
+    )
+
+    ############# Grid #####################
     logging.info("Initializing grid ...")
     nx = 50
     ny = 1
@@ -116,60 +152,12 @@ def run_tendencies(backend: str, dataset: str, output_path: str, tracking_file: 
     grid = ComputationalGrid(nx, ny, nz)
     dt = datetime.timedelta(seconds=1)
 
-    ################## Phyex #################
-    logging.info("Initializing Phyex ...")
-    cprogram = "AROME"
-    phyex = Phyex(cprogram)
-
-    ######## Backend and gt4py config #######
-    logging.info(f"With backend {backend}")
-    gt4py_config = GT4PyConfig(
-        backend=backend, rebuild=True, validate_args=False, verbose=True
-    )
-
-    ######## Instanciation + compilation #####
-    logging.info(f"Compilation for IceAdjust stencils")
-    start = time.time()
-    ice4_tendencies = Ice4Tendencies(grid, gt4py_config, phyex)
-    stop = time.time()
-    elapsed_time = stop - start
-    logging.info(f"Compilation duration for IceAdjust : {elapsed_time} s")
-
     ####### Create state for AroAdjust #######
-
     logging.info("Getting state for IceAdjust")
     state = get_constant_state_ice4_tendencies(grid, gt4py_config=gt4py_config)
-    logging.info(f"Keys : {list(state.keys())}")
 
-    ###### Launching AroAdjust ###############
-    logging.info("Launching IceAdjust")
-
-    start = time.time()
-    tends, diags = ice4_tendencies(state, dt)
-    stop = time.time()
-    elapsed_time = stop - start
-    logging.info(f"Execution duration for AroAdjust : {elapsed_time} s")
-
-    logging.info(f"Extracting state data to {output_path}")
-    output_fields = xr.Dataset(state)
-    for key, field in state.items():
-        if key not in ["time"]:
-            array = xr.DataArray(
-                data=field.data,
-                dims=["I", "J", "K"],
-                coords={
-                    "I": range(nx),
-                    "J": range(ny),
-                    "K": range(nz),
-                },
-                name=f"{key}",
-            )
-            output_fields[key] = array
-    output_fields.to_netcdf(Path(output_path))
-
-    logging.info(f"Extracting exec tracking to {tracking_file}")
-    with open(tracking_file, "w") as file:
-        json.dump(gt4py_config.exec_info, file)
+    ####### Launch execution ##################
+    core(Ice4Tendencies, gt4py_config, state, output_path, dt)
 
 
 @app.command()
