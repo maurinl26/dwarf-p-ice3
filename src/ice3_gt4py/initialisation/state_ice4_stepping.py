a664e37e19eb69370469dca362d5e9e42745910c
Loic Maurin <maurin.loic.ac@gmail.com>
2024-05-30 15:09:09 +0200

    state initialisation for ice4_stepping

---
+++ src/ice3_gt4py/initialisation/state_ice4_stepping.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from functools import partial
+from typing import TYPE_CHECKING
+
+from ifs_physics_common.framework.grid import I, J, K
+from ifs_physics_common.framework.storage import allocate_data_array
+
+from ice3_gt4py.initialisation.state import initialize_state_with_constant
+
+if TYPE_CHECKING:
+    from typing import Literal, Tuple
+
+    from ifs_physics_common.framework.config import GT4PyConfig
+    from ifs_physics_common.framework.grid import ComputationalGrid, DimSymbol
+    from ifs_physics_common.utils.typingx import (
+        DataArray,
+        DataArrayDict,
+        NDArrayLikeDict,
+    )
+
+############################## Ice4Tendencies #################################
+def allocate_state_ice4_stepping(
+    computational_grid: ComputationalGrid, gt4py_config: GT4PyConfig
+) -> NDArrayLikeDict:
+    """Allocate field to state keys following type (float, int, bool) and dimensions (2D, 3D).
+
+    Args:
+        computational_grid (ComputationalGrid): grid indexes
+        gt4py_config (GT4PyConfig): gt4py configuration
+
+    Returns:
+        NDArrayLikeDict: dictionnary of field with associated keys for field name
+    """
+
+    def _allocate(
+        grid_id: Tuple[DimSymbol, ...],
+        units: str,
+        dtype: Literal["bool", "float", "int"],
+    ) -> DataArray:
+        return allocate_data_array(
+            computational_grid, grid_id, units, gt4py_config=gt4py_config, dtype=dtype
+        )
+
+    allocate_b_ij = partial(_allocate, grid_id=(I, J), units="", dtype="bool")
+    allocate_b = partial(_allocate, grid_id=(I, J, K), units="", dtype="bool")
+    allocate_f = partial(_allocate, grid_id=(I, J, K), units="", dtype="float")
+    allocate_f_h = partial(
+        _allocate, grid_id=(I, J, K - 1 / 2), units="", dtype="float"
+    )
+    allocate_f_ij = partial(_allocate, grid_id=(I, J), units="", dtype="float")
+    allocate_i_ij = partial(_allocate, grid_id=(I, J), units="", dtype="int")
+
+    return {
+        "ldmicro": allocate_b(),
+        "exn": allocate_f(),
+        "th_t": allocate_f(),
+        "ls_fact": allocate_f(),
+        "lv_fact": allocate_f(),
+        "t": allocate_f(),
+        "rv_t": allocate_f(),
+        "rc_t": allocate_f(),
+        "rr_t": allocate_f(),
+        "ri_t": allocate_f(),
+        "rs_t": allocate_f(),
+        "rg_t": allocate_f(),
+    }
+
+
+def get_constant_state_ice4_stepping(
+    computational_grid: ComputationalGrid, *, gt4py_config: GT4PyConfig
+) -> DataArrayDict:
+    """Create state dictionnary with allocation of tables and setup to a constant value.
+
+    Args:
+        computational_grid (ComputationalGrid): grid indexes
+        gt4py_config (GT4PyConfig): configuration for gt4py
+
+    Returns:
+        DataArrayDict: initialized dictionnary of state
+    """
+    state = allocate_state_ice4_stepping(computational_grid, gt4py_config=gt4py_config)
+    initialize_state_with_constant(state, 0.5, gt4py_config, list(state.keys()))
+    return state
