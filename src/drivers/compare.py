3f79770d8f625a4bacbafad5255d44eb305ed5c3
Loic Maurin <maurin.loic.ac@gmail.com>
2024-06-05 15:05:47 +0200

    comparison

--- src/drivers/compare.py
+++ src/drivers/compare.py
@@ -1,30 +1,73 @@
+from pathlib import Path
+from typing import Dict
+import numpy as np
+import pandas as pd
+from ice3_gt4py.utils.reader import NetCDFReader
+from ifs_physics_common.utils.typingx import (
+    DataArray,
+    DataArrayDict,
+    NDArrayLikeDict,
+)
+from ice3_gt4py.initialisation.state_ice_adjust import KRR_MAPPING
 
 
-
-def inf_error(ref, run) -> float:
-    """Computes infinite error between a reference field
-    and a field to compare.
-
-    Args:
-        ref (_type_): reference field
-        run (_type_): field to compare
-
-    Returns:
-        float: infinite error
-    """
-    pass
-   
-    
-    
-def l2_norm_error(ref, run) -> float:
-    """Computes mean squared error on a field between 
-    a reference field and one for the run.
+def compare_fields(ref_path: str, run_path: str) -> Dict[str, float]:
+    """Read and compare fields in reference and run datasets and write results in output.
 
     Args:
-        ref (_type_): reference field
-        run (_type_): field to compare
-
-    Returns:
-        float: value of mse
+        ref_reader (str): path of reference dataset
+        run_reader (str): path of run dataset
+        output (str): output file to write comparison results
     """
-    pass
+    run_reader = NetCDFReader(Path(run_path))
+    ref_reader = NetCDFReader(Path(ref_path))
+    
+    inf_error = lambda ref, run: np.max(np.abs(ref - run))
+    l2_error = lambda ref, run: np.sum((ref - run)**2) / run.size    
+
+    KEYS = [
+        ("hli_hcf", "PHLI_HCF_OUT"),
+        ("hli_hri", "PHLI_HRI_OUT"),
+        ("hlc_hcf", "PHLC_HCF_OUT"),
+        ("hlc_hrc", "PHLC_HRC_OUT"),
+        ("cldfr", "PCLDFR_OUT"),
+        ("ths", "PRS_OUT"),
+        ("rvs", "PRS_OUT"),
+        ("rcs", "PRS_OUT"),
+        ("rrs", "PRS_OUT"),
+        ("ris", "PRS_OUT"),
+        ("rss", "PRS_OUT"),
+        ("rgs", "PRS_OUT")
+    ]
+    
+    tendencies = ["ths", "rvs", "rcs", "rrs", "ris", "rss", "rgs"]
+    
+    metrics = dict()
+
+    for run_name, ref_name in KEYS:
+    
+        if run_name in tendencies:
+            run_field = run_reader.get_field(run_name)
+            ref_field = ref_reader.get_field(ref_name)[:, :, tendencies.index(run_name)]
+        
+        else:
+            run_field = run_reader.get_field(run_name)
+            ref_field = ref_reader.get_field(ref_name)
+
+        e_inf = inf_error(ref_field, run_field)
+        e_l2 = l2_error(ref_field, run_field)  
+        relative_e_inf = e_inf / np.max(ref_field)
+        relative_e_l2 = ref_field.size * e_l2 / np.sum(ref_field ** 2)
+        
+        metrics.update({
+            f"{run_name}": {
+                "mean_ref": np.mean(ref_field),
+                "mean_run": np.mean(run_field),
+                "e_inf": e_inf,
+                "e_2": e_l2,
+                "relative_e_inf": relative_e_inf,
+                "relative_e_2": relative_e_l2
+            }
+       })
+            
+    return metrics    
