be6f9ec28e5f944841a12c32b21d3f9421a13c16
Loic Maurin <maurin.loic.ac@gmail.com>
2024-09-18 13:40:20 +0200

    prototype for calling Fortran and gt4py given the same arrays

--- tests/stencils/test_ice4_rrhong.py
+++ tests/stencils/test_ice4_rrhong.py
@@ -9,7 +9,7 @@ import datetime
 from ice3_gt4py.initialisation.state_rain_ice import allocate_state_ice4_rrhong
 from ice3_gt4py.initialisation.utils import initialize_field
 import ice3_gt4py.stencils
-
+import inspect
 
 
 class TestIce4RRHONG(ComputationalGridComponent):
@@ -53,13 +53,13 @@ class TestIce4RRHONG(ComputationalGridComponent):
             PTHT,
             PRRHONG_MR,
         )
-        
+
         rrhong_mr_fortran = PRRHONG_MR.copy()
-        
-        state =  allocate_state_ice4_rrhong(self.computational_grid, self.gt4py_config)
-        
+
+        state = allocate_state_ice4_rrhong(self.computational_grid, self.gt4py_config)
+
         logging.info(f"ldcompute shape {LDCOMPUTE.shape}")
-        
+
         state_ice4_rrhong = {
             "ldcompute": initialize_field(state["ldcompute"], LDCOMPUTE),
             "exn": initialize_field(state["exn"], PEXN),
@@ -68,19 +68,17 @@ class TestIce4RRHONG(ComputationalGridComponent):
             "t": initialize_field(state["t"], PT),
             "tht": initialize_field(state["tht"], PTHT),
             "rr_t": initialize_field(state["rr_t"], PRRT),
-            "rrhong_mr": initialize_field(state["rrhong_mr"], PRRHONG_MR)
+            "rrhong_mr": initialize_field(state["rrhong_mr"], PRRHONG_MR),
         }
-        
+
         self.ice4_rrhong_gt4py(
             **state_ice4_rrhong,
         )
-        
-        rrhong_mr_gt4py = PRRHONG_MR.copy() 
-        
+
+        rrhong_mr_gt4py = PRRHONG_MR.copy()
+
         diff = rrhong_mr_gt4py - rrhong_mr_fortran
         assert diff.mean() < 10e-2
-
-    
 
 
 if __name__ == "__main__":
@@ -105,7 +103,7 @@ if __name__ == "__main__":
 
     KSIZE = 15
     KPROMA = 50
-    LDCOMPUTE = np.asfortranarray(np.array([[True for _ in range(KPROMA)] for _ in range(KSIZE)]))
+    LDCOMPUTE = np.asfortranarray(np.ones((KPROMA, KSIZE)))
 
     PEXN = np.asfortranarray(np.random.rand(KPROMA, KSIZE))
     PLVFACT = np.asfortranarray(np.random.rand(KPROMA, KSIZE))
@@ -115,9 +113,37 @@ if __name__ == "__main__":
     PTHT = np.asfortranarray(np.random.rand(KPROMA, KSIZE))
     PRRHONG_MR = np.asfortranarray(np.random.rand(KPROMA, KSIZE))
 
-    ice4_rrhong = TestIce4RRHONG(
-        computational_grid=grid,
-        gt4py_config=gt4py_config,
+    mode_ice4_rrhong = fmodpy.fimport(
+        "./src/ice3_gt4py/stencils_fortran/mode_ice4_rrhong.F90"
     )
 
-    ice4_rrhong.test_feedbackt()
+    externals = {
+        "xtt": XTT,
+        "xrtmin": XRTMIN,
+        "lfeedbackt": LFEEDBACKT,
+        "kproma": KPROMA,
+        "ksize": KSIZE,
+    }
+
+    fields = {
+        "ldcompute": LDCOMPUTE,
+        "pexn": PEXN,
+        "plvfact": PLVFACT,
+        "plsfact": PLSFACT,
+        "pt": PT,
+        "prrt": PRRT,
+        "ptht": PTHT,
+        "prrhong_mr": PRRHONG_MR,
+    }
+
+    logging.info(inspect.signature(mode_ice4_rrhong.mode_ice4_rrhong.ice4_rrhong))
+    logging.info("Calling ice4_rrhong with dicts")
+
+    mode_ice4_rrhong.mode_ice4_rrhong.ice4_rrhong(**externals, **fields)
+
+    # ice4_rrhong = TestIce4RRHONG(
+    # computational_grid=grid,
+    # gt4py_config=gt4py_config,
+    # )
+
+    # ice4_rrhong.test_feedbackt()
