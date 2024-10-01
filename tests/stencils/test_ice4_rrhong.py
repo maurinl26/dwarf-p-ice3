9b6e1d1c94610f91d021ece47dd027a73ac9c0dd
Loic Maurin <maurin.loic.ac@gmail.com>
2024-10-01 09:44:18 +0200

    test stencils

--- tests/stencils/test_ice4_rrhong.py
+++ tests/stencils/test_ice4_rrhong.py
@@ -9,20 +9,18 @@ from ifs_physics_common.framework.grid import ComputationalGrid
 from ifs_physics_common.framework.grid import I, J, K
 from ifs_physics_common.framework.storage import allocate_data_array
 import datetime
-from ice3_gt4py.initialisation.utils import initialize_field
 from functools import partial
+from ice3_gt4py.phyex_common.phyex import Phyex
 import ice3_gt4py.stencils
 
-if TYPE_CHECKING:
-    from typing import Literal, Tuple
-
-    from ifs_physics_common.framework.config import GT4PyConfig
-    from ifs_physics_common.framework.grid import ComputationalGrid, DimSymbol
-    from ifs_physics_common.utils.typingx import (
-        DataArray,
-        DataArrayDict,
-        NDArrayLikeDict,
-    )
+from typing import Literal, Tuple
+from ifs_physics_common.framework.config import GT4PyConfig
+from ifs_physics_common.framework.grid import ComputationalGrid, DimSymbol
+from ifs_physics_common.utils.typingx import (
+    DataArray,
+    DataArrayDict,
+    NDArrayLikeDict,
+)
 
 
 ##### For tests ####
@@ -72,34 +70,39 @@ class TestIce4RRHONG(ComputationalGridComponent):
         self,
         computational_grid: ComputationalGrid,
         gt4py_config: GT4PyConfig,
+        phyex: Phyex,
     ) -> None:
         super().__init__(
             computational_grid=computational_grid, gt4py_config=gt4py_config
         )
 
+        externals = phyex.to_externals()
+        self.ice4_rrhong_gt4py = self.compile_stencil("ice4_rrhong", externals)
+
         self.externals = {
-            "tt": 0,
-            "r_rtmin": 10e-5,
-            "lfeedbackt": True,
+            "xtt": externals["TT"],
+            "r_rtmin": externals["R_RTMIN"],
+            "lfeedbackt": externals["LFEEDBACKT"],
         }
 
         self.dims = {"kproma": 50, "ksize": 15}
 
+        self.generate_state()
+
         # aro_filter stands for the parts before 'call ice_adjust' in aro_adjust.f90
-        self.ice4_rrhong_gt4py = self.compile_stencil("ice4_rrhong", self.externals)
 
         self.mode_ice4_rrhong = fmodpy.fimport(
             "./src/ice3_gt4py/stencils_fortran/mode_ice4_rrhong.F90"
         )
 
-    def generate_state(self, computational_grid, gt4py_config):
+    def generate_state(self):
 
-        state = allocate_state_ice4_rrhong(computational_grid, gt4py_config)
+        state = allocate_state_ice4_rrhong(self.computational_grid, self.gt4py_config)
 
-        kproma, ksize = (self.dims["kproma"],)
+        kproma, ksize = self.dims["kproma"], self.dims["ksize"]
 
-        fields = {
-            "ldcompute": np.asfortranarray(np.ones((kproma, ksize))),
+        self.fields = {
+            "ldcompute": np.asfortranarray(np.ones((kproma, ksize), dtype=np.int32)),
             "pexn": np.asfortranarray(np.random.rand(kproma, ksize)),
             "plvfact": np.asfortranarray(np.random.rand(kproma, ksize)),
             "plsfact": np.asfortranarray(np.random.rand(kproma, ksize)),
@@ -109,38 +112,35 @@ class TestIce4RRHONG(ComputationalGridComponent):
             "prrhong_mr": np.asfortranarray(np.random.rand(kproma, ksize)),
         }
 
-        state_gt4py = {
-            **{
-                key: initialize_field(state[key], fields[key])
-                for key in [
-                    "ldcompute",
-                    "exn",
-                    "ls_fact",
-                    "lv_fact",
-                    "t",
-                    "tht",
-                    "rr_t",
-                    "rrhong_mr",
-                ]
-            }
+        self.state_gt4py = {
+            "ldcompute": self.fields["ldcompute"],
+            "exn": self.fields["pexn"],
+            "ls_fact": self.fields["plsfact"],
+            "lv_fact": self.fields["plvfact"],
+            "t": self.fields["pt"],
+            "tht": self.fields["ptht"],
+            "rr_t": self.fields["prrt"],
+            "rrhong_mr": self.fields["prrhong_mr"],
         }
-
-        return fields, state_gt4py
 
     def test_feedbackt(self):
         """Call fortran stencil"""
 
-        fortran_fields, gt4py_fields = self.generate_state(
-            self.computational_grid, self.gt4py_config
-        )
+        logging.info(f"Input field, rrhong_mr : {self.fields['prrhong_mr'].mean()}")
 
         self.ice4_rrhong = self.mode_ice4_rrhong.mode_ice4_rrhong.ice4_rrhong(
-            **self.externals, **self.dims, **fortran_fields  # TODO : self.fields
+            **self.dims, **self.externals, **self.fields
         )
 
+        field_fortran = self.fields["prrhong_mr"]
+        logging.info(f"Mean Fortran {field_fortran.mean()}")
+
         self.ice4_rrhong_gt4py(
-            **gt4py_fields,
+            **self.state_gt4py,
         )
+
+        field_gt4py = self.state_gt4py["rrhong_mr"][...]
+        logging.info(f"Mean GT4Py {field_gt4py.mean()}")
 
 
 if __name__ == "__main__":
@@ -150,6 +150,8 @@ if __name__ == "__main__":
     rebuild = True
     validate_args = True
 
+    phyex = Phyex(program="AROME")
+
     # TODO : set init with grid
     logging.info("Initializing grid ...")
     grid = ComputationalGrid(50, 1, 15)
@@ -158,16 +160,11 @@ if __name__ == "__main__":
     ######## Backend and gt4py config #######
     logging.info(f"With backend {backend}")
     gt4py_config = GT4PyConfig(
-        backend=backend, rebuild=rebuild, validate_args=validate_args, verbose=True
+        backend=backend, rebuild=rebuild, validate_args=validate_args, verbose=False
     )
 
     logging.info("Calling ice4_rrhong with dicts")
 
-    ice4_rrhong = TestIce4RRHONG(
-        computational_grid=grid,
-        gt4py_config=gt4py_config,
-    )
-
-    # TODO : launch tests
-
-    # ice4_rrhong.test_feedbackt()
+    TestIce4RRHONG(
+        computational_grid=grid, gt4py_config=gt4py_config, phyex=phyex
+    ).test_feedbackt()
