269c31432b9c8ab960106b491fe45390edf6724c
root <root@LAPTOP-B31OAL7P.localdomain>
2024-10-25 16:04:20 +0200

    fields in signature closer to fortran name (without implicit typing)

--- src/ice3_gt4py/stencils/ice4_rrhong.py
+++ src/ice3_gt4py/stencils/ice4_rrhong.py
@@ -12,11 +12,11 @@ def ice4_rrhong(
     ldcompute: Field["bool"],
     t: Field["float"],
     exn: Field["float"],
-    lv_fact: Field["float"],
-    ls_fact: Field["float"],
+    lvfact: Field["float"],
+    lsfact: Field["float"],
     tht: Field["float"],  # theta at time t
     rrhong_mr: Field["float"],
-    rr_t: Field["float"],  # rain water mixing ratio at t
+    rrt: Field["float"],  # rain water mixing ratio at t
 ):
     """Compute the spontaneous frezzing source RRHONG
 
@@ -24,10 +24,10 @@ def ice4_rrhong(
         ldcompute (Field[bool]): switch to activate microphysical processes on column
         t (Field[float]): temperature at t
         exn (Field[float]): exner pressure
-        lv_fact (Field[float]): vaporisation latent heat
-        ls_fact (Field[float]): sublimation latent heat
+        lvfact (Field[float]): vaporisation latent heat
+        lsfact (Field[float]): sublimation latent heat
         tht (Field[float]): potential temperature
-        rr_t (Field[float]): rain mixing ratio at t
+        rrt (Field[float]): rain mixing ratio at t
         rrhong_mr (Field[float]): mixing ratio for spontaneous freezing source
     """
 
@@ -35,13 +35,13 @@ def ice4_rrhong(
 
     # 3.3 compute the spontaneous frezzing source: RRHONG
     with computation(PARALLEL), interval(...):
-        if t < TT - 35 and rr_t > R_RTMIN and ldcompute:
-            rrhong_mr = rr_t
+        if t < TT - 35 and rrt > R_RTMIN and ldcompute:
+            rrhong_mr = rrt
 
             # limitation for -35 degrees crossing
             if LFEEDBACKT == 1:
                 rrhong_mr = min(
-                    rrhong_mr, max(0, ((TT - 35) / exn - tht) / (ls_fact - lv_fact))
+                    rrhong_mr, max(0, ((TT - 35) / exn - tht) / (lsfact - lvfact))
                 )
 
         else:
@@ -57,10 +57,10 @@ def ice4_rrhong(
 def ice4_rrhong_post_processing(
     t: Field["float"],
     exn: Field["float"],
-    ls_fact: Field["float"],
-    lv_fact: Field["float"],
+    lsfact: Field["float"],
+    lvfact: Field["float"],
     tht: Field["float"],
-    rr_t: Field["float"],
+    rrt: Field["float"],
     rg_t: Field["float"],
     rrhong_mr: Field["float"],
 ):
@@ -69,15 +69,15 @@ def ice4_rrhong_post_processing(
     Args:
         t (Field[float]): temperature
         exn (Field[float]): exner pressure
-        ls_fact (Field[float]): sublimation latent heat over heat capacity
+        lsfact (Field[float]): sublimation latent heat over heat capacity
         tht (Field[float]): potential temperature
-        rr_t (Field[float]): rain m.r.
+        rrt (Field[float]): rain m.r.
         rg_t (Field[float]): graupel m.r.
         rrhong (Field[float]): rain m.r. increment due to homogeneous nucleation
     """
 
     with computation(PARALLEL), interval(...):
-        tht += rrhong_mr * (ls_fact - lv_fact)
+        tht += rrhong_mr * (lsfact - lvfact)
         t = tht / exn
-        rr_t -= rrhong_mr
+        rrt -= rrhong_mr
         rg_t += rrhong_mr
