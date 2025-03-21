9e7fcf648e66645dc75e8835e38ff9dc0e2fac5d
Loic Maurin <maurin.loic.ac@gmail.com>
2025-03-21 09:11:42 +0100

    repro

--- tests/repro/test_ice4_fast_ri.py
+++ tests/repro/test_ice4_fast_ri.py
@@ -7,6 +7,7 @@ from numpy.testing import assert_allclose
 from pathlib import Path
 import fmodpy
 import unittest
+from ctypes import c_float
 
 import logging
 
@@ -14,89 +15,83 @@ from .env import BACKEND, REBUILD, VALIDATE_ARGS, SHAPE
 
 
 class TestIce4Fast(unittest.TestCase):
-    
     def test_ice4_fast_ri(self):
-        
         logging.info(f"With backend {BACKEND}")
         gt4py_config = GT4PyConfig(
-            backend=BACKEND, 
-            rebuild=REBUILD, 
-            validate_args=VALIDATE_ARGS, 
+            backend=BACKEND,
+            rebuild=REBUILD,
+            validate_args=VALIDATE_ARGS,
             verbose=True,
-            dtypes=DataTypes(bool=bool, float=np.float32, int=np.int32)
+            dtypes=DataTypes(bool=bool, float=np.float32, int=np.int32),
         )
-        
-        logging.info(f"Machine precision {np.finfo(np.float32).eps}")
-        logging.info(f"Machine precision {np.finfo(np.float32).eps}")
 
+        logging.info(f"Machine precision {np.finfo(np.float32).eps}")
+        logging.info(f"Machine precision {np.finfo(np.float32).eps}")
 
         phyex_externals = Phyex("AROME").to_externals()
         ice4_fast_ri_gt4py = compile_stencil(
-            "ice4_fast_ri", 
-            gt4py_config, 
-            phyex_externals
+            "ice4_fast_ri", gt4py_config, phyex_externals
         )
-        
-        
+
         ldcompute = np.ones(
-                (SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=bool,
-                order="F",
-            )
+            (SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=bool,
+            order="F",
+        )
         rhodref = np.array(
-                np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
-        ai= np.array(
-                np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
-        cj= np.array(
-                np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
-        cit= np.array(
-                np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
-        ssi= np.array(
-                np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
-        rct= np.array(
-                np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
-        rit= np.array(
-                np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
+            np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
+        ai = np.array(
+            np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
+        cj = np.array(
+            np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
+        cit = np.array(
+            np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
+        ssi = np.array(
+            np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
+        rct = np.array(
+            np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
+        rit = np.array(
+            np.random.rand(SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
         rc_beri_tnd = np.zeros(
-                (SHAPE[0], SHAPE[1], SHAPE[2]),
-                dtype=np.float32,
-                order="F",
-            )
+            (SHAPE[0], SHAPE[1], SHAPE[2]),
+            dtype=c_float,
+            order="F",
+        )
         ldsoft = False
-        
+
         ldcompute_gt4py = from_array(ldcompute, dtype=bool, backend=BACKEND)
         rhodref_gt4py = from_array(rhodref, dtype=np.float32, backend=BACKEND)
         ai_gt4py = from_array(ai, dtype=np.float32, backend=BACKEND)
         cj_gt4py = from_array(cj, dtype=np.float32, backend=BACKEND)
-        cit_gt4py = from_array(cit, dtype=np.float32, backend=BACKEND) 
+        cit_gt4py = from_array(cit, dtype=np.float32, backend=BACKEND)
         ssi_gt4py = from_array(ssi, dtype=np.float32, backend=BACKEND)
         rct_gt4py = from_array(rct, dtype=np.float32, backend=BACKEND)
         rit_gt4py = from_array(rit, dtype=np.float32, backend=BACKEND)
         rc_beri_tnd_gt4py = from_array(rc_beri_tnd, dtype=np.float32, backend=BACKEND)
-        
+
         logging.info(f"IN mean rc_beri_tnd_gt4py {rc_beri_tnd_gt4py.mean()}")
-        
+
         ice4_fast_ri_gt4py(
             ldcompute=ldcompute_gt4py,
             rhodref=rhodref_gt4py,
@@ -105,30 +100,27 @@ class TestIce4Fast(unittest.TestCase):
             cit=cit_gt4py,
             ssi=ssi_gt4py,
             rct=rct_gt4py,
-            rit=rit_gt4py, 
+            rit=rit_gt4py,
             rc_beri_tnd=rc_beri_tnd_gt4py,
-            ldsoft=ldsoft
+            ldsoft=ldsoft,
         )
-        
+
         fortran_script = "mode_ice4_fast_ri.F90"
         current_directory = Path.cwd()
         root_directory = current_directory
         stencils_directory = Path(
-            root_directory, 
-            "src", 
-            "ice3_gt4py", 
-            "stencils_fortran"
+            root_directory, "src", "ice3_gt4py", "stencils_fortran"
         )
         script_path = Path(stencils_directory, fortran_script)
 
         logging.info(f"Fortran script path {script_path}")
         fortran_script = fmodpy.fimport(script_path)
-        
+
         logging.info(f"IN mean rc_beri_tnd {rc_beri_tnd.mean()}")
-                
+
         result = fortran_script.mode_ice4_fast_ri.ice4_fast_ri(
-            kproma=SHAPE[0]*SHAPE[1]*SHAPE[2],
-            ksize=SHAPE[0]*SHAPE[1]*SHAPE[2],
+            kproma=SHAPE[0] * SHAPE[1] * SHAPE[2],
+            ksize=SHAPE[0] * SHAPE[1] * SHAPE[2],
             c_rtmin=phyex_externals["C_RTMIN"],
             i_rtmin=phyex_externals["I_RTMIN"],
             xlbexi=phyex_externals["LBEXI"],
@@ -145,15 +137,15 @@ class TestIce4Fast(unittest.TestCase):
             pssi=ssi.ravel(),
             prct=rct.ravel(),
             prit=rit.ravel(),
-            prcberi=rc_beri_tnd.ravel()
+            prcberi=rc_beri_tnd.ravel(),
         )
 
         rcberi_out = result[0]
-        
+
         logging.info(f"Mean rc_beri_tnd_gt4py   {rc_beri_tnd_gt4py.mean()}")
         logging.info(f"Mean rcberi_out          {rcberi_out.mean()}")
-        logging.info(f"Max abs rtol             {max(abs(rc_beri_tnd_gt4py.ravel() - rcberi_out) / abs(rcberi_out))}")
-        
+        logging.info(
+            f"Max abs rtol             {max(abs(rc_beri_tnd_gt4py.ravel() - rcberi_out) / abs(rcberi_out))}"
+        )
 
         assert_allclose(rc_beri_tnd_gt4py.ravel(), rcberi_out, 1e-5)
-
