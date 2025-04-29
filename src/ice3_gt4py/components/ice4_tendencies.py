afc50ff22ea50a4209bc8f5d33439b66eb7cd0e8
Loïc Maurin <loicmaurin@Mac-mini-de-Loic.local>
2025-04-29 22:46:51 +0200

    ruff formatting

--- src/ice3_gt4py/components/ice4_tendencies.py
+++ src/ice3_gt4py/components/ice4_tendencies.py
@@ -45,6 +45,7 @@ class Ice4Tendencies(ImplicitTendencyComponent):
         self.gaminc_rim4 = phyex.rain_ice_param.GAMINC_RIM4
 
 
+
         # Tendencies
         self.ice4_nucleation = self.compile_stencil("ice4_nucleation", externals)
         self.ice4_nucleation_post_processing = self.compile_stencil(
