2fb5668a7cce58b2e16ac20a1f1927b58ab9794a
Loic Maurin <maurin.loic.ac@gmail.com>
2025-05-27 14:59:39 +0200

    dependency to cupy removed

--- src/drivers/core.py
+++ src/drivers/core.py
@@ -56,12 +56,6 @@ def write_dataset(state: DataArrayDict, shape: Tuple[int], output_path: str):
     nx, ny, nz = shape
 
     logging.info(f"Extracting state data to {output_path}")
-    logging.info(f"Type, state {type(state)}")
-    logging.info(f"Type, state {type(state['rhodref'])}")
-    # logging.info(f"Is cupy, state {state['rhodref'].cupy.is_cupy}")
-
-    state['rhodref'] = state['rhodref'].as_cupy()
-
     output_fields = xr.Dataset(state)
     for key, field in state.items():
         if key not in ["time"]:
