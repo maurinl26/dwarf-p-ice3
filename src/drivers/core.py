52db990b4554ea5a903f044ba33e37644e4e8894
Loic Maurin <maurin.loic.ac@gmail.com>
2025-05-27 13:38:36 +0200

    as cupy conversion

--- src/drivers/core.py
+++ src/drivers/core.py
@@ -59,6 +59,9 @@ def write_dataset(state: DataArrayDict, shape: Tuple[int], output_path: str):
     logging.info(f"Type, state {type(state)}")
     logging.info(f"Type, state {type(state['rhodref'])}")
     logging.info(f"Is cupy, state {state['rhodref'].cupy.is_cupy}")
+
+    state['rhodref'] = state['rhodref'].as_cupy()
+
     output_fields = xr.Dataset(state)
     for key, field in state.items():
         if key not in ["time"]:
