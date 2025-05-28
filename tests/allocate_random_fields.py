0ffd9b6a0b2dce27cc53cf1361e2200d0d0b47dd
Loic Maurin <maurin.loic.ac@gmail.com>
2025-05-28 15:54:54 +0200

    cleaning fields passing to gt4py

---
+++ tests/allocate_random_fields.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from ifs_physics_common.framework.config import GT4PyConfig
+from ifs_physics_common.framework.grid import Grid
+from ctypes import c_float, c_double
+from typing import List, Dict
+import gt4py
+
+
+def draw_fields(names: List[str], gt4py_config: GT4PyConfig, grid: Grid):
+    return {
+        name: np.array(
+            np.random.rand(*grid.shape),
+            dtype=(c_float if gt4py_config.dtypes.float == np.float32 else c_double),
+            order="F",
+        )
+        for name in names
+    }
+
+def allocate_gt4py_fields(names: List[str], gt4py_config: GT4PyConfig, grid: Grid):
+    return {
+        name: gt4py.storage.zeros(
+            shape=grid.shape,
+            dtype=gt4py_config.dtypes.float,
+            backend=gt4py_config.backend
+        )
+        for name in names
+    }
+
+def allocate_fields(gt4py_fields, buffer):
+    # Allocate
+    for name in gt4py_fields.keys():
+        gt4py_fields[name] = buffer[name]
+
+    return gt4py_fields
+
+def allocate_fortran_fields(
+        f2py_names: Dict[str],
+        buffer: Dict[np.ndarray]
+):
+    return {
+        fname: buffer[pyname].ravel()
+        for fname, pyname in f2py_names.items()
+    }
