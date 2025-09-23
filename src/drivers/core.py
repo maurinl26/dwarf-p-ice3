e1449e2031869b86cc44ef98621bf89563e3d4dc
maurinl26 <maurin.loic.ac@gmail.com>
2025-09-23 09:32:01 +0200

    ice3_gt4py renamed ice3

--- src/drivers/core.py
+++ src/drivers/core.py
@@ -16,8 +16,6 @@ from ice3_gt4py.phyex_common.phyex import Phyex
 
 from ifs_physics_common.utils.typingx import (
     DataArrayDict,
-    NDArrayLikeDict, PropertyDict
-
 )
 
 from ice3_gt4py.utils.reader import NetCDFReader
