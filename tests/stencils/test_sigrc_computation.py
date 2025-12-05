d386395cdca557e28b10885c9721d97cbe78640e
Loic Maurin <maurin.loic.ac@gmail.com>
2025-12-05 16:30:47 +0100

    cleaning sigrc_computation

---
+++ tests/stencils/test_sigrc_computation.py
@@ -0,0 +1,10 @@
+import cupy as cp
+
+
+def test_cupy_take_interpolation():
+    """
+    Test 1D interpolation with cupy take and 
+    a switch between numpy take and cupy take, whether the 
+    backend is cpu or gpu.
+    """
+    ...
