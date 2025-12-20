d1174a3a50c30f503fe76f6726b89142ad776646
maurinl26 <maurin.loic.ac@gmail.com>
2025-12-20 11:35:21 +0100

    adding an ice_adjust_cython wrapper

--- PHYEX-IAL_CY50T1/bridge/_phyex_wrapper.pyx
+++ PHYEX-IAL_CY50T1/bridge/_phyex_wrapper.pyx
@@ -1,58 +1,188 @@
 # distutils: language = c
 # cython: language_level = 3
+"""
+Cython wrapper for PHYEX ICE_ADJUST routine.
+
+This module provides a Python interface to the Fortran ICE_ADJUST subroutine
+through a C bridge defined in phyex_bridge.F90.
+"""
 import numpy as np
 cimport numpy as np
 
-# Declare memory view data types for double precision (JPRB/float64)
-ctypedef np.float64_t DTYPE_t
+# Declare memory view data types for single precision (JPRB/float32)
+ctypedef np.float32_t DTYPE_t
 
-# 1. External C Declaration
-# This matches the signature defined in the Fortran bridge:
-# SUBROUTINE c_ice_adjust_wrap(nlon, nlev, ptr_pt, ptr_pq, ptr_pp) BIND(C, name="c_ice_adjust")
+# External C Declaration matching phyex_bridge.F90
 cdef extern:
     void c_ice_adjust(
-        int nlon, 
+        int nlon,
         int nlev, 
-        double *ptr_pt, 
-        double *ptr_pq, 
-        double *ptr_pp
+        int krr,
+        float timestep,
+        float *ptr_sigqsat,
+        float *ptr_pabs,
+        float *ptr_sigs,
+        float *ptr_th,
+        float *ptr_exn,
+        float *ptr_exn_ref,
+        float *ptr_rho_dry_ref,
+        float *ptr_rv,
+        float *ptr_rc,
+        float *ptr_ri,
+        float *ptr_rr,
+        float *ptr_rs,
+        float *ptr_rg,
+        float *ptr_cf_mf,
+        float *ptr_rc_mf,
+        float *ptr_ri_mf,
+        float *ptr_rvs,
+        float *ptr_rcs,
+        float *ptr_ris,
+        float *ptr_ths,
+        float *ptr_cldfr,
+        float *ptr_icldfr,
+        float *ptr_wcldfr
     )
 
-# 2. Python-Callable Wrapper Function
+# Python-Callable Wrapper Function
 def ice_adjust(
-    # Use C-contiguous memory view for the fastest possible access.
-    # We must ensure the input NumPy arrays are F-contiguous (Fortran order)
-    # as Fortran uses column-major order. Cython can handle the translation
-    # to C pointers easily.
-    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] pt, 
-    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] pq, 
-    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] pp
+    # Scalar parameters
+    float timestep,
+    int krr,
+    # 1D arrays (Fortran-contiguous)
+    np.ndarray[DTYPE_t, ndim=1, mode="fortran"] sigqsat,
+    # 2D arrays (Fortran-contiguous, shape: nlon × nlev)
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] pabs,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] sigs,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] th,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] exn,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] exn_ref,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rho_dry_ref,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rv,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rc,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] ri,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rr,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rs,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rg,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] cf_mf,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rc_mf,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] ri_mf,
+    # Tendency arrays (input/output, modified in-place)
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rvs,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] rcs,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] ris,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] ths,
+    # Output arrays (modified in-place)
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] cldfr,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] icldfr,
+    np.ndarray[DTYPE_t, ndim=2, mode="fortran"] wcldfr
 ):
     """
     Cython wrapper for the PHYEX ICE_ADJUST routine.
-    Input arrays must be 2D, Fortran-contiguous (KLON, KLEV), and float64.
-    PT and PQ are modified in-place.
+    
+    Performs saturation adjustment for mixed-phase clouds, computing
+    condensation/deposition and updating mixing ratios and temperature.
+    
+    Parameters
+    ----------
+    timestep : float
+        Time step (seconds)
+    krr : int
+        Number of moist variables (typically 6 or 7)
+    sigqsat : ndarray (nlon,)
+        Standard deviation of saturation mixing ratio
+    pabs : ndarray (nlon, nlev)
+        Absolute pressure (Pa)
+    sigs : ndarray (nlon, nlev)
+        Sigma_s for subgrid turbulent mixing
+    th : ndarray (nlon, nlev)
+        Potential temperature (K)
+    exn : ndarray (nlon, nlev)
+        Exner function
+    exn_ref : ndarray (nlon, nlev)
+        Reference Exner function
+    rho_dry_ref : ndarray (nlon, nlev)
+        Reference dry air density (kg/m³)
+    rv, rc, ri, rr, rs, rg : ndarray (nlon, nlev)
+        Mixing ratios for vapor, cloud liquid, cloud ice, rain, snow, graupel (kg/kg)
+    cf_mf, rc_mf, ri_mf : ndarray (nlon, nlev)
+        Mass flux cloud fraction and mixing ratios
+    rvs, rcs, ris, ths : ndarray (nlon, nlev)
+        Tendency fields (modified in-place)
+    cldfr, icldfr, wcldfr : ndarray (nlon, nlev)
+        Output cloud fractions (modified in-place)
+    
+    Returns
+    -------
+    None
+        All output arrays are modified in-place.
+    
+    Notes
+    -----
+    - All 2D arrays must be Fortran-contiguous with shape (nlon, nlev)
+    - All arrays must be float32 (np.float32) for single precision
+    - Tendency arrays (rvs, rcs, ris, ths) are both input and output
+    - Cloud fraction arrays (cldfr, icldfr, wcldfr) are output only
+    
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from _phyex_wrapper import ice_adjust
+    >>> 
+    >>> # Initialize arrays (Fortran order, single precision)
+    >>> nlon, nlev = 10, 20
+    >>> sigqsat = np.ones(nlon, dtype=np.float32, order='F') * 0.01
+    >>> pabs = np.ones((nlon, nlev), dtype=np.float32, order='F') * 85000.0
+    >>> # ... initialize other arrays ...
+    >>> 
+    >>> # Call ice_adjust
+    >>> ice_adjust(
+    ...     timestep=60.0, krr=6,
+    ...     sigqsat=sigqsat, pabs=pabs, sigs=sigs, th=th,
+    ...     exn=exn, exn_ref=exn_ref, rho_dry_ref=rho_dry_ref,
+    ...     rv=rv, rc=rc, ri=ri, rr=rr, rs=rs, rg=rg,
+    ...     cf_mf=cf_mf, rc_mf=rc_mf, ri_mf=ri_mf,
+    ...     rvs=rvs, rcs=rcs, ris=ris, ths=ths,
+    ...     cldfr=cldfr, icldfr=icldfr, wcldfr=wcldfr
+    ... )
     """
     
-    # 3. Get the dimensions
-    cdef int nlon = pt.shape[0]
-    cdef int nlev = pt.shape[1]
-
-    # Type check for safety (handled by Cython but good practice)
-    if (pq.shape[0] != nlon or pq.shape[1] != nlev or 
-        pp.shape[0] != nlon or pp.shape[1] != nlev):
-        raise ValueError("All input arrays must have the same (KLON, KLEV) shape.")
-
-    # 4. Call the Fortran function using C pointers
-    # We access the raw data buffer (address of the first element)
+    # Get dimensions
+    cdef int nlon = pabs.shape[0]
+    cdef int nlev = pabs.shape[1]
+    
+    # Validate 1D array shapes
+    if sigqsat.shape[0] != nlon:
+        raise ValueError("sigqsat shape mismatch: expected ({},), got ({},)".format(
+            nlon, sigqsat.shape[0]))
+    
+    # Validate 2D array shapes
+    cdef list arrays_2d = [
+        ('pabs', pabs), ('sigs', sigs), ('th', th), ('exn', exn),
+        ('exn_ref', exn_ref), ('rho_dry_ref', rho_dry_ref),
+        ('rv', rv), ('rc', rc), ('ri', ri), ('rr', rr), ('rs', rs), ('rg', rg),
+        ('cf_mf', cf_mf), ('rc_mf', rc_mf), ('ri_mf', ri_mf),
+        ('rvs', rvs), ('rcs', rcs), ('ris', ris), ('ths', ths),
+        ('cldfr', cldfr), ('icldfr', icldfr), ('wcldfr', wcldfr)
+    ]
+    
+    for name, arr in arrays_2d:
+        if arr.shape[0] != nlon or arr.shape[1] != nlev:
+            raise ValueError(
+                "{} shape mismatch: expected ({}, {}), got ({}, {})".format(
+                    name, nlon, nlev, arr.shape[0], arr.shape[1])
+            )
+    
+    # Call the Fortran function through C bridge
     c_ice_adjust(
-        nlon, 
-        nlev, 
-        &pt[0, 0],  # Pointer to the start of pt array
-        &pq[0, 0],  # Pointer to the start of pq array
-        &pp[0, 0]   # Pointer to the start of pp array
+        nlon, nlev, krr, timestep,
+        &sigqsat[0],
+        &pabs[0, 0], &sigs[0, 0], &th[0, 0], &exn[0, 0], &exn_ref[0, 0],
+        &rho_dry_ref[0, 0], &rv[0, 0], &rc[0, 0], &ri[0, 0],
+        &rr[0, 0], &rs[0, 0], &rg[0, 0],
+        &cf_mf[0, 0], &rc_mf[0, 0], &ri_mf[0, 0],
+        &rvs[0, 0], &rcs[0, 0], &ris[0, 0], &ths[0, 0],
+        &cldfr[0, 0], &icldfr[0, 0], &wcldfr[0, 0]
     )
-
-    # Note: Cython modified the arrays IN-PLACE, so we don't need to return anything new.
-    # We return the inputs for convenience, but they are the same objects passed in.
-    return pt, pq
+    
+    # Arrays are modified in-place, no return needed
--- PHYEX-IAL_CY50T1/bridge/phyex_bridge.F90
+++ PHYEX-IAL_CY50T1/bridge/phyex_bridge.F90
@@ -1,30 +1,212 @@
 MODULE phyex_bridge
     USE ISO_C_BINDING
-    ! Import the original routine and precision module
+    ! Import the original routine and required modules
     USE MODI_ICE_ADJUST, ONLY : ICE_ADJUST
     USE PARKIND1, ONLY : JPIM, JPRB
+    USE MODD_DIMPHYEX, ONLY : DIMPHYEX_t
+    USE MODD_CST, ONLY : CST_t, CST
+    USE MODD_RAIN_ICE_PARAM_n, ONLY : RAIN_ICE_PARAM_t
+    USE MODD_NEB_n, ONLY : NEB_t
+    USE MODD_TURB_n, ONLY : TURB_t
+    USE MODD_PARAM_ICE_n, ONLY : PARAM_ICE_t
+    USE MODD_BUDGET, ONLY : TBUDGETCONF_t, TBUDGETDATA_PTR
+    USE MODE_INI_CST, ONLY : INI_CST
 
     IMPLICIT NONE
 
 CONTAINS
 
-    ! This function is callable from Python (C compatible)
-    SUBROUTINE c_ice_adjust_wrap(nlon, nlev, ptr_pt, ptr_pq, ptr_pp) BIND(C, name="c_ice_adjust")
-        ! 1. Define C-compatible arguments
-        INTEGER(C_INT), VALUE, INTENT(IN) :: nlon, nlev
-        TYPE(C_PTR), VALUE, INTENT(IN)    :: ptr_pt, ptr_pq, ptr_pp
-
-        ! 2. Define Fortran Pointers to map the data
-        REAL(KIND=JPRB), POINTER, DIMENSION(:,:) :: f_pt, f_pq, f_pp
+    ! C-callable wrapper for ICE_ADJUST
+    SUBROUTINE c_ice_adjust_wrap(                                          &
+        nlon, nlev, krr, timestep,                                         &
+        ptr_sigqsat, ptr_pabs, ptr_sigs, ptr_th, ptr_exn, ptr_exn_ref,    &
+        ptr_rho_dry_ref, ptr_rv, ptr_rc, ptr_ri, ptr_rr, ptr_rs, ptr_rg,  &
+        ptr_cf_mf, ptr_rc_mf, ptr_ri_mf,                                   &
+        ptr_rvs, ptr_rcs, ptr_ris, ptr_ths,                                &
+        ptr_cldfr, ptr_icldfr, ptr_wcldfr                                  &
+    ) BIND(C, name="c_ice_adjust")
+    
+        ! C-compatible arguments (using C_FLOAT for single precision)
+        INTEGER(C_INT), VALUE, INTENT(IN) :: nlon, nlev, krr
+        REAL(C_FLOAT), VALUE, INTENT(IN) :: timestep
         
-        ! 3. Convert C Pointers to Fortran Arrays (No data copy!)
-        CALL C_F_POINTER(ptr_pt, f_pt, [nlon, nlev])
-        CALL C_F_POINTER(ptr_pq, f_pq, [nlon, nlev])
-        CALL C_F_POINTER(ptr_pp, f_pp, [nlon, nlev])
+        ! C pointers for input arrays
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_sigqsat      ! 1D: (nlon)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_pabs         ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_sigs         ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_th           ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_exn          ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_exn_ref      ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rho_dry_ref  ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rv           ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rc           ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_ri           ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rr           ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rs           ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rg           ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_cf_mf        ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rc_mf        ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_ri_mf        ! 2D: (nlon, nlev)
+        
+        ! C pointers for input/output tendency arrays
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rvs          ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_rcs          ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_ris          ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_ths          ! 2D: (nlon, nlev)
+        
+        ! C pointers for output arrays
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_cldfr        ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_icldfr       ! 2D: (nlon, nlev)
+        TYPE(C_PTR), VALUE, INTENT(IN) :: ptr_wcldfr       ! 2D: (nlon, nlev)
 
-        ! 4. Call the actual PHYEX routine
-        ! We assume KIDIA=1, KFDIA=nlon for the whole array
-        CALL ICE_ADJUST(1, nlon, nlon, nlev, f_pt, f_pq, f_pp)
+        ! Fortran pointers to map C data (using C_FLOAT for single precision)
+        REAL(KIND=C_FLOAT), POINTER, DIMENSION(:) :: f_sigqsat
+        REAL(KIND=C_FLOAT), POINTER, DIMENSION(:,:) :: f_pabs, f_sigs, f_th, f_exn, f_exn_ref
+        REAL(KIND=C_FLOAT), POINTER, DIMENSION(:,:) :: f_rho_dry_ref, f_rv, f_rc, f_ri
+        REAL(KIND=C_FLOAT), POINTER, DIMENSION(:,:) :: f_rr, f_rs, f_rg
+        REAL(KIND=C_FLOAT), POINTER, DIMENSION(:,:) :: f_cf_mf, f_rc_mf, f_ri_mf
+        REAL(KIND=C_FLOAT), POINTER, DIMENSION(:,:) :: f_rvs, f_rcs, f_ris, f_ths
+        REAL(KIND=C_FLOAT), POINTER, DIMENSION(:,:) :: f_cldfr, f_icldfr, f_wcldfr
+        
+        ! Local variables for PHYEX structures
+        TYPE(DIMPHYEX_t) :: D
+        TYPE(RAIN_ICE_PARAM_t) :: ICEP
+        TYPE(NEB_t) :: NEBN
+        TYPE(TURB_t) :: TURBN
+        TYPE(PARAM_ICE_t) :: PARAMI
+        TYPE(TBUDGETCONF_t) :: BUCONF
+        TYPE(TBUDGETDATA_PTR), DIMENSION(0) :: TBUDGETS
+        
+        ! Additional required arrays (using C_FLOAT)
+        REAL(KIND=C_FLOAT), ALLOCATABLE, DIMENSION(:,:) :: PRHODJ, PZZ
+        REAL(KIND=C_FLOAT), ALLOCATABLE, DIMENSION(:,:) :: PMFCONV
+        REAL(KIND=C_FLOAT), ALLOCATABLE, DIMENSION(:,:) :: PWEIGHT_MF_CLOUD
+        REAL(KIND=C_FLOAT), ALLOCATABLE, DIMENSION(:,:) :: PSSIO, PSSIU, PIFR
+        REAL(KIND=C_FLOAT), ALLOCATABLE, DIMENSION(:,:) :: PSRCS
+        LOGICAL :: LMFCONV, OCOMPUTE_SRC
+        
+        ! Convert C pointers to Fortran arrays
+        CALL C_F_POINTER(ptr_sigqsat, f_sigqsat, [nlon])
+        CALL C_F_POINTER(ptr_pabs, f_pabs, [nlon, nlev])
+        CALL C_F_POINTER(ptr_sigs, f_sigs, [nlon, nlev])
+        CALL C_F_POINTER(ptr_th, f_th, [nlon, nlev])
+        CALL C_F_POINTER(ptr_exn, f_exn, [nlon, nlev])
+        CALL C_F_POINTER(ptr_exn_ref, f_exn_ref, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rho_dry_ref, f_rho_dry_ref, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rv, f_rv, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rc, f_rc, [nlon, nlev])
+        CALL C_F_POINTER(ptr_ri, f_ri, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rr, f_rr, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rs, f_rs, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rg, f_rg, [nlon, nlev])
+        CALL C_F_POINTER(ptr_cf_mf, f_cf_mf, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rc_mf, f_rc_mf, [nlon, nlev])
+        CALL C_F_POINTER(ptr_ri_mf, f_ri_mf, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rvs, f_rvs, [nlon, nlev])
+        CALL C_F_POINTER(ptr_rcs, f_rcs, [nlon, nlev])
+        CALL C_F_POINTER(ptr_ris, f_ris, [nlon, nlev])
+        CALL C_F_POINTER(ptr_ths, f_ths, [nlon, nlev])
+        CALL C_F_POINTER(ptr_cldfr, f_cldfr, [nlon, nlev])
+        CALL C_F_POINTER(ptr_icldfr, f_icldfr, [nlon, nlev])
+        CALL C_F_POINTER(ptr_wcldfr, f_wcldfr, [nlon, nlev])
+        
+        ! Initialize DIMPHYEX structure
+        D%NIT = nlon
+        D%NIB = 1
+        D%NIE = nlon
+        D%NJT = 1
+        D%NJB = 1
+        D%NJE = 1
+        D%NKT = nlev
+        D%NKL = 1        ! Ground to space ordering
+        D%NKA = 1
+        D%NKU = nlev
+        D%NKB = 1
+        D%NKE = nlev
+        D%NKTB = 1
+        D%NKTE = nlev
+        D%NIBC = 1
+        D%NJBC = 1
+        D%NIEC = nlon
+        D%NJEC = 1
+        D%NIJT = nlon
+        D%NIJB = 1
+        D%NIJE = nlon
+        D%NKLES = nlev
+        D%NLESMASK = 0
+        D%NLES_TIMES = 0
+        
+        ! Initialize physical constants (uses global CST module)
+        CALL INI_CST()
+        
+        ! Initialize NEBN (nebulosity/cloud parameters) - AROME defaults
+        NEBN%LSUBG_COND = .FALSE.     ! No subgrid condensation
+        NEBN%LSIGMAS = .TRUE.         ! Use sigma_s
+        NEBN%CFRAC_ICE_ADJUST = 'S'   ! Standard
+        NEBN%CCONDENS = 'CB02'        ! Condensation scheme
+        NEBN%CLAMBDA3 = 'CB'          ! Lambda3 formulation
+        
+        ! Initialize PARAMI (ice parameters)
+        PARAMI%CSUBG_MF_PDF = 'NONE'  ! No mass flux PDF
+        PARAMI%LOCND2 = .FALSE.       ! OCND2 option
+        
+        ! Initialize TURBN (turbulence parameters)
+        ! Set minimal required fields
+        
+        ! Initialize ICEP (rain/ice parameters)
+        ! Set minimal required fields
+        
+        ! Initialize budget configuration (disable budgets)
+        BUCONF%LBUDGET_TH = .FALSE.
+        BUCONF%LBUDGET_RV = .FALSE.
+        BUCONF%LBUDGET_RC = .FALSE.
+        BUCONF%LBUDGET_RI = .FALSE.
+        
+        ! Allocate and initialize additional required arrays
+        ALLOCATE(PRHODJ(nlon, nlev))
+        ALLOCATE(PZZ(nlon, nlev))
+        ALLOCATE(PMFCONV(nlon, nlev))
+        ALLOCATE(PWEIGHT_MF_CLOUD(nlon, nlev))
+        ALLOCATE(PSSIO(nlon, nlev))
+        ALLOCATE(PSSIU(nlon, nlev))
+        ALLOCATE(PIFR(nlon, nlev))
+        ALLOCATE(PSRCS(nlon, nlev))
+        
+        ! Compute PRHODJ from density and assume unit Jacobian
+        PRHODJ = f_rho_dry_ref
+        
+        ! Set height field (simplified - could be passed as parameter)
+        PZZ = 0.0_C_FLOAT
+        
+        ! Initialize mass flux arrays
+        PMFCONV = 0.0_C_FLOAT
+        PWEIGHT_MF_CLOUD = 0.0_C_FLOAT
+        LMFCONV = .FALSE.
+        
+        ! Initialize output arrays
+        PSSIO = 0.0_C_FLOAT
+        PSSIU = 0.0_C_FLOAT
+        PIFR = 0.0_C_FLOAT
+        PSRCS = 0.0_C_FLOAT
+        OCOMPUTE_SRC = .FALSE.
+        
+        ! Call the actual ICE_ADJUST routine (using global CST module)
+        CALL ICE_ADJUST(                                                   &
+            D, CST, ICEP, NEBN, TURBN, PARAMI, BUCONF, krr,                &
+            'BRID',                                                        &
+            timestep, f_sigqsat,                                           &
+            PRHODJ, f_exn_ref, f_rho_dry_ref, f_sigs, LMFCONV, PMFCONV,   &
+            f_pabs, PZZ,                                                   &
+            f_exn, f_cf_mf, f_rc_mf, f_ri_mf, PWEIGHT_MF_CLOUD,            &
+            f_icldfr, f_wcldfr, PSSIO, PSSIU, PIFR,                        &
+            f_rv, f_rc, f_rvs, f_rcs, f_th, f_ths,                         &
+            OCOMPUTE_SRC, PSRCS, f_cldfr,                                  &
+            f_rr, f_ri, f_ris, f_rs, f_rg, TBUDGETS, 0                     &
+        )
+        
+        ! Cleanup
+        DEALLOCATE(PRHODJ, PZZ, PMFCONV, PWEIGHT_MF_CLOUD)
+        DEALLOCATE(PSSIO, PSSIU, PIFR, PSRCS)
 
     END SUBROUTINE c_ice_adjust_wrap
 
